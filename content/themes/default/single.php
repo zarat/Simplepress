4cc675a0dbaaecc1b836c3f8f794f8f4e49e3853
zarat <manuel.zarat@gmail.com>
2018-01-05 18:40:55 +0100

    Update single.php

--- content/themes/default/single.php
+++ content/themes/default/single.php
@@ -1,5 +1,19 @@
 <?php
 
-print_r($system->settings());
+$menu = new menu();
+$menu->config(array('id' => 1));
+$menu->html();
+
+$item = $system->single($type=$system->the_querystring('type'),$id=$system->the_querystring('id')); 
+
+echo "<div class='content'>";
+
+echo "<div class ='content-item-head'>" . $item['title'] . "</div>";
+echo "<div class ='content-item-head-secondary'>Kategorie: " . $system->single($type='category',$id=$item['category'])['title'] . "</div>";
+echo "<div class ='content-item-body'>" . html_entity_decode($item['content']) . "</div>";
+
+echo "</div>";
+
+include "sidebar.php";
 
 ?>
