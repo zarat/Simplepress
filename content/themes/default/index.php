b31fae29781eec26f3aba35438d6bbe6e2320e12
zarat <manuel.zarat@gmail.com>
2018-01-05 18:40:09 +0100

    Update index.php

--- content/themes/default/index.php
+++ content/themes/default/index.php
@@ -1,1 +1,21 @@
-i am <?php echo __file__; ?>
+<?php
+
+$menu = new menu();
+$menu->config(array('id' => 1));
+$menu->html();
+
+echo "<div class='content'>\n\n";
+
+foreach($system->archive($what="*",$from="object",$where="type='post' ORDER BY id DESC") as $item) {
+
+echo "\t<div class='content-item'>\n";
+    echo "\t\t<div class='content-item-head'><a href='../?type=$item[type]&id=$item[id]'>" . $item['title'] . "</a></div>\n";
+    echo "\t\t<div class='content-item-body'>" . substr(strip_tags(html_entity_decode($item['content'])),0,150) . "\n\t\t</div>\n";
+echo "\t</div>\n\n";
+}
+
+echo "</div>";
+
+include "sidebar.php";
+
+?>
