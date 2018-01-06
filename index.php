f9314ba7cdf259433c54dc08a68db2d8d8e4efb5
zarat <manuel.zarat@gmail.com>
2018-01-06 05:07:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php 
 
-define('DS', DIRECTORY_SEPARATOR);
-define('ABSPATH', dirname(__file__) . DS);
-
 require 'load.php';
 
 $project = new system();
+
 $theme = new theme();
 
 $theme->set_include('header','<title>project</title>');
 $theme->set_include('header','<script src="https://simplepress.ml/admin/js/admin.js"></script>');
-$theme->set_include('header','<link rel="stylesheet" href="../content/themes/default/css/style.css">');
+$theme->set_include('header','<link rel="stylesheet" href="../content/themes/' . $project->settings('site_theme') . '/css/style.css">');
+$theme->set_include('header','<link rel="stylesheet" href="../content/themes/' . $project->settings('site_theme') . '/css/menu.css">');
+$theme->set_include('header',"<script src='../content/themes/" . $project->settings('site_theme') . "/js/externallinks.js'></script>");
 $theme->set_include('footer','<!-- powered by qwerty -->');
 
 $theme->display_page();
