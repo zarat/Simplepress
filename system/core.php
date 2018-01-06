4fa86518107cbf504017b943466fbfd93f664df7
zarat <manuel.zarat@gmail.com>
2018-01-06 05:03:54 +0100

    Update core.php

--- system/core.php
+++ system/core.php
@@ -1,19 +1,28 @@
 <?php
 
-/*
-Abstrakte Klasse kann nicht initialisiert, nur abgeleitet werden.
-*/
-abstract class core {
+/**
+ * @author Manuel Zarat
+ * @date 05.01.2018
+ * 
+ */
 
-    //abstract function required_function();               
+abstract class core {               
  
     private $db = false;  
-     
+
+    /**
+     * Datenbankverbindung aufbauen
+     * 
+     */
     final function __construct() {          
         if(is_file(ABSPATH . "config.php")) { include ABSPATH . "config.php"; } else { include ABSPATH . "install.php"; exit(); }                
         $this->db = new mysqli($dbhost,$dbuser,$dbpass,$dbname);                                                 
     }
     
+    /**
+     * Datenbankinteraktionen
+     * 
+     */
     private function sql_escape_string($query) {
         return mysqli_real_escape_string($this->db, $query);    
     }        
@@ -28,9 +37,13 @@ abstract class core {
         return $sql->fetch_assoc();
     }
     
-    /*
-    Gemeinsame Methoden aller ableitenden Klassen (können teilweise überschrieben werden)
-    */ 
+    /**
+     * Einstellungen aus der Tabelle settings
+     * 
+     * @param string $key
+     * @return string/array
+     * 
+     */
     final function settings($key=null) {
         $query = (null!==$key) ? "SELECT * FROM settings WHERE settings.key='$key'" : "SELECT * FROM settings";
         $result = $this->query($query);
@@ -40,7 +53,15 @@ abstract class core {
         return (null !== $key) ? $ret[$key] : $ret;
     }
     
-    function archive($select,$from,$where) {
+    /**
+     * Archiv von Objekten
+     * 
+     * @param array $config
+     * @return array
+     * 
+     */
+    function archive($config) {
+        extract($config);
         $items = $this->query("SELECT $select FROM $from WHERE $where");
         $result = false;
         while($item = $this->fetch($items)) {
@@ -49,6 +70,13 @@ abstract class core {
         return $result;
     }
     
+    /**
+     * Einzelnes Objekt
+     * 
+     * @param int $id
+     * @return array
+     * 
+     */
     function single($type,$id) {
         $item = $this->fetch($this->query("SELECT * FROM object WHERE type='$type' AND id=$id"));
         return ($item) ? $item : array('error' => 'no such object');
