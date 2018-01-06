f91131f9253714393d44a9826afe5e3bfb14908f
zarat <manuel.zarat@gmail.com>
2018-01-06 05:04:52 +0100

    Update menu.php

--- system/classes/menu.php
+++ system/classes/menu.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @author Manuel Zarat
+ * @date 05.01.2018
+ * 
+ */
+
 class menu extends system {
 
 private $menu_id;
@@ -7,12 +13,25 @@ private $divclass;
 private $ulclass; 
 private $sublevel = 0;
 
+    /** 
+     * Laedt das Menue
+     *
+     * @param array $config
+     * @return void
+     */
     function config($config) {
         $this->menu_id = $config['id'];  
     }
     
+    /**
+     * Rekursive Funktion, die sich selbst immer und immer wieder aufruft
+     * 
+     * @param id int
+     * @return string
+     */
     protected function items($id=0) {
-        if($parents = $this->archive($select="*", $from="menu", $where="menu_id=" . $this->menu_id . " AND parent=$id ORDER BY sort")) {
+        $query = array('select' => '*','from' => 'menu','where' => "menu_id=" . $this->menu_id . " AND parent=$id ORDER BY sort");
+        if($parents = $this->archive($query)) {
             if($this->sublevel<1) { echo "\n" . str_repeat("\t", $this->sublevel) . "<ul class='menu level-$this->sublevel'>\n"; }
             else { echo "\n" . str_repeat("\t", $this->sublevel) . "<ul class='submenu level-$this->sublevel'>\n"; } 
             $this->sublevel = $this->sublevel+1;  
@@ -28,6 +47,11 @@ private $sublevel = 0;
         }
     }
     
+    /**
+     * Anzeigen
+     * 
+     * @return string
+     */
     public function html() {
         $this->divclass = "nav-container";    
         echo "<div class='$this->divclass'>";
