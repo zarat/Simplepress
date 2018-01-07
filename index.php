d00510f1c609af50f913fc16b253db2a6b76d14e
zarat <manuel.zarat@gmail.com>
2018-01-07 04:31:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php 
 
+/**
+ * @author Manuel Zarat
+ * @date 05.01.2018
+ * @license http://opensource.org/licenses/MIT
+ *
+ * Klasse core und autoloader einbinden.
+ * 
+ */
 require 'load.php';
 
-$project = new system();
-
-$theme = new theme();
-
-$theme->set_include('header','<title>project</title>');
-$theme->set_include('header','<script src="https://simplepress.ml/admin/js/admin.js"></script>');
-$theme->set_include('header','<link rel="stylesheet" href="../content/themes/' . $project->settings('site_theme') . '/css/style.css">');
-$theme->set_include('header','<link rel="stylesheet" href="../content/themes/' . $project->settings('site_theme') . '/css/menu.css">');
-$theme->set_include('header',"<script src='../content/themes/" . $project->settings('site_theme') . "/js/externallinks.js'></script>");
-$theme->set_include('footer','<!-- powered by qwerty -->');
-
-$theme->display_page();
+$simplepress = new system();
+$simplepress->init();
 
 ?>
