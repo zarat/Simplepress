17a8747e693abe43658b9e9cfa4fe9af60049fa5
zarat <manuel.zarat@gmail.com>
2018-01-07 04:50:26 +0100

    Update functions.php

--- content/themes/simplepress/functions.php
+++ content/themes/simplepress/functions.php
@@ -1,8 +1,6 @@
 <?php
 
 /**
- * Vorlage eines Themes
- *
  * @author Manuel Zarat
  * @date 05.01.2018
  * @license http://opensource.org/licenses/MIT
@@ -11,8 +9,6 @@
  *
  */
 
-//global $theme;
-
 /**
  * Includes sind dazu da, an bestimmten Positionen innerhalb eines Themes Inhalte einfuegen.
  * 
@@ -20,18 +16,6 @@
 $this->set_include('header','<title>SimplePress beta</title>');
 $this->set_include('header','<link rel="stylesheet" href="../content/themes/simplepress/css/style.css">');
 $this->set_include('header','<link rel="stylesheet" href="../content/themes/simplepress/css/menu.css">');
-$this->set_include('footer','<!-- powered by qwerty -->');
-
-/**
- * Trigger sind dazu da, um zu bestimmten Zeitpunkten waehrend des Seitenaufbau Funktionen aufrufen zu koennen.
- * 
- */
-
-/**
- * Filter sind dazu da, um Funktionen im Theme zu ueberladen bzw ueberschreiben zu koennen.
- * 
- */
-  
-
+$this->set_include('footer','<!-- powered by Simplepress - https://github.com/zarat/simplepress -->');
 
 ?>
