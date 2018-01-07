47788a83a0dda5bff5797759251b1703cdb8a590
zarat <manuel.zarat@gmail.com>
2018-01-07 04:33:49 +0100

    Update theme.php

--- system/classes/theme.php
+++ system/classes/theme.php
@@ -1,51 +1,113 @@
 <?php
 
-/**
+/** 
+ * SimplePress Theme Dummy
+ *
  * @author Manuel Zarat
  * @date 05.01.2018
+ * @license http://opensource.org/licenses/MIT
+ * 
+ * @todo THEMES sollten eine Datei "theme.php" in Root Verzeichnis beinhalten, die im Fall dieses Dummy Theme ueberschreibt.
+ * @todo THEMES sollten einen Unterordner "<THEME-DIR>/views" beinhalten, in dem eigene VIEWS definiert werden koennen.
  * 
  */
 
 class theme extends system {
 
-    private $includes = array('header','footer');
-    private $variables = array();
+    /**
+     * THEMES muessen VIEWS definieren, die vorgeben, welche Positionen ein Theme hat.
+     * So koennen auch Themes mit individuellen VIEWS arbeiten (ShoppingCart, Listen, Profil, ...)
+     *
+     * @todo Eigene VIEWS sollen manuell erstellbar sein
+     * 
+     */
+    private $views = array('single','archive','index');
+    
+    /**
+     * Themes muessen Positionen aufweisen, in welche man Inhalte einfuegen kann.
+     * 
+     * @see self::set_include() 
+     *
+     */
+    private $positions = array('header','footer');
+
+    /**
+     * Zu bestimmten Zeiten waehrend dem Seitenaufbau werden SYSTEM Events geworfen.
+     * 
+     * @todo Events rufen Trigger auf, die den Aufbau zur Laufzeit erweitern sollen.
+     * 
+     */
+    private $events = array();
+    
+    /**
+     * Zu jedem registrierten Event werden zugehoerige Trigger aufgerufen.
+     * 
+     * @todo Eigene Trigger sollen registriert werden koennen.
+     * 
+     */
+    private $triggers = array();
     
     /**
      * Registriert Inhalte zum Einbinden an bestimmten Positionen (Hooks ->)
+     * 
+     * @todo
      * 
      * @param string $position Wo soll es eingefuegt werden
      * @param string $include Was soll eingefuegt werden
      * 
      * @return void
      * 
+     * Example: set_include('header','i am the header');
      */
-    final function set_include($position,$include) {
-        $this->include_positions[$position][] = $include;
+    function set_include($position,$include) {
+        //$this->include_positions[$position][] = $include;
+        $this->views[$position][] = $include;
     }
     
     /**
-     * Zeigt einzubindende Inhalte zu einer Position an
+     * Zeigt einzubindende Inhalte zu einem Event an.
+     * 
+     * @todo 
+     * @testing
      * 
      * @param string position
-     * @return void
      * 
      */
-    final function get_includes($position) {
-        foreach($this->include_positions[$position] as $include) {
+    function get_includes($position) {
+        foreach($this->views[$position] as $include) {
             echo $include . "\n";
         }
     }
     
     /**
-     *  Bindet angeforderte Objekt Templates ein
+     * Setzt einen Trigger zu einem bestimmten Event
+     * 
+     * @todo Soll das Event ueberschreiben oder ueberladen koennen.
+     *
+     */
+     function set_trigger($event, $trigger) { }
+     
+     /**
+      * Ruft Trigger zu einem bestimmten Event auf.
+      * 
+      * @todo
+      *
+      */
+    function execute_triggers($event) { }
+    
+    /**
+     *  Bindet das angeforderte View Template ein (archiv,single,custom..)
      *  Muss aufgeteilt werden um Hooks einzubauen.
      *  
-     * @return void
+     * @todo
+     * @deprecated
+     *  
+     * @return false
+     * 
      */ 
     function object_path() { 
-        $this->type = !empty($this->the_querystring('type')) ? $this->the_querystring('type') : "default"; // default
-        $this->id = !empty($this->the_querystring('id')) ? $this->the_querystring('id') : false; // 0
+        $this->type = !empty($this->request('type')) ? $this->request('type') : "default"; // default
+        $this->id = !empty($this->request('id')) ? $this->request('id') : false; // 0
         switch($this->type) {
             case "post":
             case "page":
@@ -60,18 +122,16 @@ class theme extends system {
                 $this->view = "default"; 
                 break;
         }
+        
         $system = new system();
+        
         switch($this->view) {        
             case "single": 
                 $item = $this->single($this->type,$this->id); 
-                if(is_file(ABSPATH . "content" . DS . "themes" . DS . $this->settings('site_theme') . DS . "single-" . $this->type . ".php")) {
-                
-                    include ABSPATH . "content" . DS . "themes" . DS . $this->settings('site_theme') . DS . "single-" . $this->type . ".php";
-                    
-                } else {
-                
-                    include ABSPATH . "content" . DS . "themes" . DS . $this->settings('site_theme') . DS . "single.php";
-                    
+                if(is_file(ABSPATH . "content" . DS . "themes" . DS . $this->settings('site_theme') . DS . "single-" . $this->type . ".php")) {                
+                    include ABSPATH . "content" . DS . "themes" . DS . $this->settings('site_theme') . DS . "single-" . $this->type . ".php";                    
+                } else {                
+                    include ABSPATH . "content" . DS . "themes" . DS . $this->settings('site_theme') . DS . "single.php";                   
                 }
             break;
             case "archive":
@@ -83,27 +143,53 @@ class theme extends system {
         }    
         return false;      
     }
-    
+
     /**
-     * Ausgabe
+     * Ausgabe aller Komponenten
+     * Die einzelnen Schritte sind in Funktionen aufgeteilt, um Trigger aufrufen zu koennen.
+     * 
+     * @todo
      * 
      * @return string
      * 
      */
-    final function display_page() {
+    function render() {
     
+        /** Theme Settings importieren */
+        include ABSPATH . 'content' . DS . 'themes' . DS . $this->settings('site_theme') . DS . 'functions.php';
+    
+        $this->get_header();
+
+        echo "<div class='main-wrapper'>\n";
+        
+        $this->object_path();
+        
+        echo "</div>";
+        
+        $this->get_footer();
+    
+    }
+    
+    /**
+     * Gibt den HTML Header aus
+     * 
+     */
+    function get_header() {
         echo "<html>\n";
         echo "<head>\n";
         $this->get_includes('header');
         echo "</head>\n";
-        echo "<body>\n";
-        echo "<div class='main-wrapper'>\n";
-        $this->object_path();
-        echo "</div>";
-        echo "\n</body>\n";
-        $this->get_includes('footer');
-        echo "</html>\n";
+        echo "<body>";
+    }
     
+    /**
+     * Gibt den HTML Footer aus.
+     * 
+     */
+    function get_footer() {
+        $this->get_includes('footer');
+        echo "</body>\n";
+        echo "</html>";
     }
 
 }
