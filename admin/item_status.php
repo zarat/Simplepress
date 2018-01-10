53cfc400e180ed08e1ad58aacd1353e765d76814
zarat <manuel.zarat@gmail.com>
2018-01-10 10:47:09 +0100

    Update item_status.php

--- admin/item_status.php
+++ admin/item_status.php
@@ -1,34 +1,20 @@
 <?php
 
+if(!isset($_GET['id']) || !isset($_GET['status'])) { die("error: missing parameter 'id' or 'status'"); }
+
 include("../load.php");
 
 $system = new system();
 
-	if(!isset($_GET['id']) || !isset($_GET['status'])) {
-		echo "error";
-		exit;
-	}
-
-
 	  $id = $_GET['id'];
 		$status = $_GET['status'];
     
-    if($status == '0') {
-        $var = "<b>deaktiviert</b>";
-    } else {
-        $var = "<b>aktiviert</b>";
-    }
-    
-		//$update = "UPDATE object SET status=$status WHERE id=$id"; 
-    
-    ($status==1)?$newstatus=0:$newstatus=1;
-    ($status==1)?$v="deaktivieren":$v="aktivieren";
-    
-		//$db->query($update); 
+    $newstatus = ($status==1) ? 0 : 1;
+    $var = ($status==1) ? "deaktivieren" : "aktivieren";
     
     $config = array("table"=>"object","set"=>"status=$status WHERE id=$id");
-    $system->update_object($config);
+    $system->update($config);
     
-    echo "<a style='cursor:pointer' onclick=\"update_status('$id','$newstatus')\">$v</a>";		
+    echo "<a style='cursor:pointer' onclick=\"update_status('$id','$newstatus')\">$var</a>";		
 
 ?>
