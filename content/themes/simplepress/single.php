29b670ac82a4fee347bf2aaccc76012dc440fae3
zarat <manuel.zarat@gmail.com>
2018-01-11 22:31:10 +0100

    Update single.php

--- content/themes/simplepress/single.php
+++ content/themes/simplepress/single.php
@@ -5,19 +5,25 @@
  * 
  */ 
 
-echo "<div class='content'>\n";
+echo "<div class='sp-content'>\n";
 
 if($item = $system->single(array('type'=>$system->request('type'),'id'=>$system->request('id')))) { 
-    echo "<div class ='content-item-head'>" . $item['title'] . "</div>\n";
-    echo "<div class ='content-item-body'>" . html_entity_decode($item['content']) . "</div>\n";
+
+    echo "<div class ='sp-content-item'>\n";
+    echo "<div class ='sp-content-item-head'>" . $item['title'] . "</div>\n";
+    echo "<div class ='sp-content-item-body'>" . html_entity_decode($item['content']) . "</div>\n";
+    echo "</div>\n";
+    
 } else {
-    echo "<div class ='content-item-head'>" . $system->_t('no_items_to_display') . "</div>\n";
+
+    echo "<div class ='sp-content-item'>\n";
+    echo "<div class ='sp-content-item-head'>" . $system->_t('no_items_to_display') . "</div>\n";
+    echo "</div>\n";
+    
 }
 
 if(isset($item['attachment'])) { echo "<img src='" . $item['attachment'] . "'>"; }
 
 echo "</div>\n";
-
-//include "sidebar.php";
 
 ?>
