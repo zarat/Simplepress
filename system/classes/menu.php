e2903ec03e2999e323e0e77473cdad06f4a5b29a
zarat <manuel.zarat@gmail.com>
2018-01-11 04:09:08 +0100

    Add files via upload

---
+++ system/classes/menu.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @author Manuel Zarat
+ * 
+ */
+
+class menu extends system {
+
+private $menu_id;
+private $div;
+private $ul; 
+private $sublevel = 0;
+
+    /** 
+     * Laedt das Menue
+     *
+     * @param array $config
+     * @return void
+     */
+    function config($config) {
+        $this->menu_id = $config['id'];
+        $this->div = "nav-container";
+        $this->ul = "submenu";
+    }
+    
+    /**
+     * Rekursive Funktion, die sich selbst immer und immer wieder aufruft
+     * 
+     * @param id int
+     * @return string
+     */
+    protected function items($id=0) {
+        $query = array('select' => '*','from' => 'menu','where' => "menu_id=" . $this->menu_id . " AND parent=$id ORDER BY sort");
+        if($parents = $this->archive($query)) {
+        
+            if($this->sublevel<1) { echo "\n" . str_repeat("\t", $this->sublevel) . "<ul class='menu level-$this->sublevel'>\n"; } /** Noch kin Submenu!!! */
+            else { echo "\n" . str_repeat("\t", $this->sublevel) . "<ul class='$this->ul level-$this->sublevel'>\n"; } 
+            $this->sublevel = $this->sublevel+1;  
+            foreach($parents as $item) {    
+                echo str_repeat("\t", $this->sublevel) ."<li><a href='$item[link]'>$item[label]</a>";
+                if($children = $this->items($item['id'])) {
+                    echo $children;
+                }
+                echo str_repeat("\t", $this->sublevel) . "</li>\n";
+            } 
+            $this->sublevel = $this->sublevel-1;
+            /**
+             * Adminlink bei letzter Iteration
+             * 
+             */
+            if($this->sublevel<1 && isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == 1) { 
+                echo str_repeat("\t", $this->sublevel) . "<li><a href='../admin'>Admin</a></li>\n"; 
+            }       
+            echo str_repeat("\t", $this->sublevel) . "</ul>\n";
+            
+        }
+    }
+    
+    /**
+     * Anzeigen
+     * 
+     * @return string
+     */
+    public function html() {    
+        echo "<div class='$this->div'>";
+        echo "<div>";
+        echo "<label class='responsive_menu' for='responsive_menu'>";
+        echo "<span>Menu</span>";
+        echo "</label>";
+        echo "<input id='responsive_menu' type='checkbox'>"; 
+        $this->items();                  
+        echo "</div>\n\n";
+    }
+
+}
+
+?>
---
+++ system/classes/system.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * @author Manuel Zarat
+ * 
+ */
+
+class system extends core {
+
+    /**
+     * Ein THEME, das definiert, wie der Seitenaufbau ablaeuft und aussieht.
+     * Das Theme muss zur Laufzeit initiiert werden, kommt aber aus der DB
+     * 
+     * @var object Theme Objekt
+     * 
+     */
+    private $theme = false;
+    
+    /**
+     * Eintrittsfunktion
+     * Hier koennen Objekte global initiiert werden.
+     * 
+     */
+    final function init() {    
+        $this->setup_theme();
+        $this->theme->render();    
+    }
+    
+    /**
+     * Wenn ein Theme File mit dem Namen wie das Theme heisst '<THEME-DIR>/<THEME-NAME>.php' existiert (was es sollte!) wird es hier eingebunden und initiiert.
+     * 
+     * Das ganze muss aber natuerlich auch laufen, wenn ein Theme kaputt oder geloescht ist.
+     * 
+     * Ansonsten wird ein Dummy Theme initiiert.
+     * 
+     * @todo Was wenn das Theme nicht funktioniert? Fallback?
+     *
+     * @return false
+     *
+     */
+    private function setup_theme() {
+        /**
+         * Pruefe, ob Themedatei existiert
+         * 
+         */
+        if(is_file($custom_theme_file=ABSPATH . 'content' . DS . 'themes' . DS . $this->settings('site_theme') . DS . 'theme.php')) {
+            /**
+             * Wenn ja, includen und Theme initiieren
+             * 
+             */
+            include $custom_theme_file; 
+            $custom_theme = $this->settings('site_theme');
+            $this->theme = new $custom_theme;
+        } else {
+            /**
+             * Wenn nicht, wird das Theme Dummy aufgerufen
+             * Stylesheet u.a fehlen dann aber..
+             * Nochmal auf settings.php pruefen? Dzt in theme::default_header()
+             * @see theme.php
+             *
+             */
+            $this->theme = new theme();
+        }
+        return false;               
+    }
+    
+    /**
+     * Der Querystring ist wegen URL-rewriting (spaeter) wichtig
+     *
+     * @access public
+     * @param string Fuer einen bestimmten Parameter
+     * @return array/string/false
+     * 
+     */
+    final function request($key=false) {
+        if($_SERVER['QUERY_STRING']) {
+            parse_str($_SERVER['QUERY_STRING'], $parameters);
+            if(false !== $key && !empty($parameters[$key])) {
+                return $parameters[$key];
+            } else {
+                return $parameters;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * @todo Prueft dzt nur, welche Ordner im Ordner ../content/themes/* enthalten sind.
+     * 
+     */
+    function installed_themes() {
+        $themes = null;        
+        if ($files = opendir( ABSPATH . 'content' . DS . 'themes')) {        
+            while (false !== ($file = readdir($files))) { if ($file!='.' && $file!='..'){ $themes[] = $file; } }        
+            closedir($files);    
+        }        
+        return $themes;    
+    }
+    
+    /**
+     * Uebersetzt einen String aus einer Sprachdatei  ../system/lang/*
+     * 
+     * @todo
+     * 
+     */
+    function _t($str) {
+        if($this->settings('site_language')) {
+            include ABSPATH . 'system' . DS . 'lang' . DS . 'lang-' . $this->settings('site_language') . '.php';
+        } else {
+            include ABSPATH . 'system' . DS . 'lang' . DS . 'lang.php';
+        }
+        return isset($lang[$str]) ? $lang[$str] : "Language file is missing or corrupt.";
+    }
+
+}
+
+?>
