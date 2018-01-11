477ed762df49f40e5455e60cd15beac8e63e997c
zarat <manuel.zarat@gmail.com>
2018-01-11 16:52:08 +0100

    Add files via upload

---
+++ system/lang/lang-de.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Simplepress Sprachdatei
+ * 
+ */
+
+/**
+ * Installation
+ * 
+ */
+$lang['system_not_installed'] = "Es scheint, Simplepress ist nicht installiert.";
+$lang['please_enter_setup_details'] = "Bitte gib deine Serverdaten ein und klicke auf installieren.";
+
+/**
+ * Config pages
+ *
+ */
+$lang['welcome_to_dashboard'] = "Willkommen im Dashboard";
+$lang['dashboard_description'] = "Das ist die Komandozentrale deiner Website.";
+$lang['change_website_title'] = "Titel deiner Website";
+$lang['change_website_subtitle'] = "Subtitel deiner Website";
+$lang['change_website_theme'] = "Design deiner Website";
+$lang['change_website_keywords'] = "Schlagworte deiner Website";
+$lang['change_website_description'] = "Kurzbeschreibung deiner Website";
+
+/**
+ * Item add
+ * 
+ */
+$lang['item_add'] = "Item anlegen";
+$lang['item_add_title'] = "Titel";
+$lang['item_add_category'] = "Kategorie"; 
+$lang['item_add_keywords'] = "Schlagworte";
+$lang['item_add_description'] = "Beschreibung";
+$lang['item_add_content'] = "Inhalt";
+
+/**
+ * Item modify
+ * 
+ */
+$lang['item_modify'] = "Item bearbeiten";
+$lang['item_modify_title'] = "Titel";
+$lang['item_modify_category'] = "Kategorie"; 
+$lang['item_modify_keywords'] = "Schlagworte";
+$lang['item_modify_description'] = "Beschreibung";
+$lang['item_modify_content'] = "Inhalt";
+
+/**
+ * Betrieb
+ * 
+ */
+$lang['no_items_to_display'] = "Tut mir leid, dazu konnte ich nichts finden.";
+
+?>
---
+++ system/lang/lang.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Simplepress Sprachdatei
+ * 
+ */
+
+/**
+ * Installation
+ * 
+ */
+$lang['system_not_installed'] = "It seems, Simplepress is not installed.";
+$lang['please_enter_setup_details'] = "Please enter your server settings.";
+
+/**
+ * Dashboard
+ *
+ */
+$lang['welcome_to_dashboard'] = "Welcome to the dashboard";
+$lang['dashboard_description'] = "From here you can manage all your content";
+$lang['change_website_title'] = "Change the title of your website";
+$lang['change_website_subtitle'] = "Change the subtitle of your website";
+$lang['change_website_theme'] = "Change the design of your website";
+$lang['change_website_keywords'] = "Change the keywords of your website";
+$lang['change_website_description'] = "Change the description of your website";
+
+/**
+ * Item add
+ * 
+ */
+$lang['item_add'] = "Add new item";
+$lang['item_add_title'] = "Title";
+$lang['item_add_category'] = "Category"; 
+$lang['item_add_keywords'] = "Keywords";
+$lang['item_add_description'] = "Description";
+$lang['item_add_content'] = "Content";
+
+/**
+ * Item modify
+ * 
+ */
+$lang['item_modify'] = "Edit item";
+$lang['item_modify_title'] = "Title";
+$lang['item_modify_category'] = "Category"; 
+$lang['item_modify_keywords'] = "Keywords";
+$lang['item_modify_description'] = "Description";
+$lang['item_modify_content'] = "Content";
+ 
+/**
+ * Betrieb
+ * 
+ */
+$lang['no_items_to_display'] = "Sorry, we could not find anything.";
+
+?>
