3d65c6305b93db504bd6b52dda53e24c5ac3967b
zarat <manuel.zarat@gmail.com>
2018-01-11 22:30:52 +0100

    Update single-page.php

--- content/themes/simplepress/single-page.php
+++ content/themes/simplepress/single-page.php
@@ -7,17 +7,19 @@
 
 $item = $system->single(array('type'=>$system->request('type'),'id'=>$system->request('id'))); 
 
-echo "<div class='content'>\n";
+echo "<div class='sp-content'>\n";
 
 if($item) { 
-    echo "<div class ='content-item-head'>" . $item['title'] . "</div>\n";
-    echo "<div class ='content-item-body'>" . html_entity_decode($item['content']) . "</div>\n";
+
+    echo "<div class ='sp-content-item-head'>" . $item['title'] . "</div>\n";
+    echo "<div class ='sp-content-item-body'>" . html_entity_decode($item['content']) . "</div>\n";
+    
 } else {
-    echo "<div class ='content-item-head'>" . $system->_t('no_items_to_display') . "</div>\n";
+
+    echo "<div class ='sp-content-item-head'>" . $system->_t('no_items_to_display') . "</div>\n";
+    
 }
 
 echo "</div>\n";
-
-//include "sidebar.php";
 
 ?>
