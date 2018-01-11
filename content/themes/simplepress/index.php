adfd42c321c76d651fbeca7a1084e4ad352851a8
zarat <manuel.zarat@gmail.com>
2018-01-11 22:30:11 +0100

    Update index.php

--- content/themes/simplepress/index.php
+++ content/themes/simplepress/index.php
@@ -5,20 +5,27 @@
  * 
  */
 
-echo "<div class='content'>\n\n";
+echo "<div class='sp-content'>\n\n";
 
 $conf = array('select' => '*','from' => 'object', 'where' => 'type="post" AND status=1 ORDER BY id DESC');
+
 if($r = $system->archive($conf)) {
-foreach($r as $item) {
 
-echo "\t<div class='content-item'>\n";
-    echo "\t\t<div class='content-item-head'><a href='../?type=$item[type]&id=$item[id]'>" . $item['title'] . "</a></div>\n";
-    echo "\t\t<div class='content-item-body'>" . substr(strip_tags(html_entity_decode($item['content'])),0,150) . "\n\t\t</div>\n";
-echo "\t</div>\n\n";
-
-}
+    foreach($r as $item) {
+    
+    echo "\t<div class='sp-content-item'>\n";
+        echo "\t\t<div class='sp-content-item-head'><a href='../?type=$item[type]&id=$item[id]'>" . $item['title'] . "</a></div>\n";
+        echo "\t\t<div class='sp-content-item-body'>" . substr(strip_tags(html_entity_decode($item['content'])),0,150) . "\n\t\t</div>\n";
+    echo "\t</div>\n\n";
+    
+    }
+    
 } else {
-    echo "<div class ='content-item-head'>" . $system->_t('no_items_to_display') . "</div>\n";
+
+    echo "\t<div class='sp-content-item'>\n";
+        echo "<div class ='sp-content-item-head'>" . $system->_t('no_items_to_display') . "</div>\n";
+    echo "\t</div>\n\n";
+    
 }
 
 echo "</div>\n\n";
