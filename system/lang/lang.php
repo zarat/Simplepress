609cd05d238230937eeca3bf28a02821e882b3c8
zarat <manuel.zarat@gmail.com>
2018-01-11 04:10:02 +0100

    Add files via upload

---
+++ system/lang/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Simplepress Sprachdatei
+ * 
+ */
+
+/**
+ * Installation
+ * 
+ */
+$lang['system_not_installed'] = "It seems, Simplepress is not installed.";
+$lang['please_enter_setup_details'] = "Please enter your server settings.";
+
+/**
+ * Betrieb
+ * 
+ */
+$lang['no_items_to_display'] = "Sorry, we could not find anything.";
+
+?>
