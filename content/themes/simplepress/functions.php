d07edf380b5bd650e6bef42336b7c5dd0c460b8c
zarat <manuel.zarat@gmail.com>
2018-01-13 14:50:42 +0100

    Update functions.php

--- content/themes/simplepress/functions.php
+++ content/themes/simplepress/functions.php
@@ -11,14 +11,6 @@
  * Includes sind dazu da, an bestimmten Positionen innerhalb eines Themes Inhalte einfuegen.
  * 
  */
-$this->set_include("header","<title>SimplePress beta</title>");
-
-$this->set_include("header","<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />");
-$this->set_include("header","<meta name='viewport' content='width=device-width, initial-scale=1.0' />");
-$this->set_include("header","<meta name='generator' content='SimplePress - https://github.com/zarat/simplepress' />");
-$this->set_include("header","<meta name='description' content='Mein neues CMS'>");
-
-$this->set_include("header","<link rel='stylesheet' href='../content/themes/simplepress/css/style.css'>");
 $this->set_include("header","<link rel='stylesheet' href='../content/themes/simplepress/css/menu.css'>");
 $this->set_include("header","<script src='../content/themes/simplepress/js/externallinks.js'></script>");
 
