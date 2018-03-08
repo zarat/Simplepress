903b0085d6cc87f7d513809dfcae9e30086630f0
mbrain <simplepresscms@gmail.com>
2018-03-08 11:59:59 +0100

    added parameters

--- content/themes/simplepress/archive.php
+++ content/themes/simplepress/archive.php
@@ -13,11 +13,11 @@ echo "<div class='sp-content'>\n";
 
 while( $archive->have_posts() ) {
 
-    $item = $archive->the_post();
+    $item = $archive->the_post( $strip_tags=true, $content_length=200 );
     
     echo "<div class='sp-content-item'>\n";
     echo "<div class='sp-content-item-head'><a href='../?type=$item[type]&id=$item[id]'>" . $item['title'] . "</a></div>\n";
-    echo "<div class='sp-content-item-body'>" . substr(strip_tags(html_entity_decode($item['content'])),0,150) . "</div>\n";
+    echo "<div class='sp-content-item-body'>" . $item['content'] . "</div>\n";
     echo "</div>\n";
     
 }
