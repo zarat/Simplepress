ae57d52c001eae1956a8b7af3e22647ac9176c70
mbrain <simplepresscms@gmail.com>
2018-03-08 11:59:04 +0100

    the_post() added parameters

--- content/themes/simplepress/index.php
+++ content/themes/simplepress/index.php
@@ -9,11 +9,11 @@ echo "<div class='sp-content'>\n\n";
 
 while( $latest->have_posts() ) {
 
-    $item = $latest->the_post();
+    $item = $latest->the_post( $strip_tags=true, $content_length=200 );
     
     echo "<div class='sp-content-item'>\n";
     echo "<div class='sp-content-item-head'><a href='../?type=$item[type]&id=$item[id]'>" . $item['title'] . "</a></div>\n";
-    echo "<div class='sp-content-item-body'>" . substr(strip_tags(html_entity_decode($item['content'])),0,150) . "</div>\n";
+    echo "<div class='sp-content-item-body'>" . $item['content'] . "</div>\n";
     echo "</div>\n";
     
 }
