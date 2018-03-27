d4c783b519db0b4ce3279154c8f7bd5c615fcda6
zarat <manuel.zarat@gmail.com>
2018-03-27 17:06:57 +0200

    Update sidebar.php

--- content/themes/simplepress/sidebar.php
+++ content/themes/simplepress/sidebar.php
@@ -5,15 +5,22 @@
  * 
  */
  
-$conf = array('select' => 'id,title','from' => 'object','where' => 'type="category"');
+echo "<div class='sp-sidebar-item'>";
+    echo "<div class='sp-sidebar-item-head'>Suche</div>";
+    echo "<div class='sp-sidebar-item-box'>\n";
+        echo "<div class='sp-sidebar-item-box-head'><div class='container'><form><input type='hidden' name='type' value='search'><input type='text' name='term'></form></div></div>\n";
+    echo "</div>\n";
+echo "</div>";
+ 
+$conf = array('select' => 'id,title','from' => 'object','where' => 'status=1 AND type="category"');
 
-echo "<div class='sp-sidebar'>\n";
-    echo "<div class='sp-sidebar-item'>";
-        echo "<div class='sp-sidebar-item-head'>Kategorien</div>";
-        foreach($system->archive($conf) as $cat) {
-            echo "<div class='sp-sidebar-item-box'>\n<div class='sp-sidebar-item-box-head'><a href='../?type=category&id=$cat[id]'>$cat[title]</a></div>\n</div>\n";
-        }
-    echo "</div>";
-echo "</div>\n";
+echo "<div class='sp-sidebar-item'>";
+    echo "<div class='sp-sidebar-item-head'>Kategorien</div>";
+    foreach($system->archive($conf) as $cat) {
+        echo "<div class='sp-sidebar-item-box'>\n";
+            echo "<div class='sp-sidebar-item-box-head'><a href='../?type=category&id=$cat[id]'>$cat[title]</a></div>\n";
+        echo "</div>\n";
+    }
+echo "</div>";
 
 ?>
