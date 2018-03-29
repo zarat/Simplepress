6a5d76f9d51560fba6e3d357178756b6f0a15ce1
zarat <manuel.zarat@gmail.com>
2018-03-29 12:02:02 +0200

    Update sidebar.php

--- content/themes/simplepress/sidebar.php
+++ content/themes/simplepress/sidebar.php
@@ -8,7 +8,7 @@
 echo "<div class='sp-sidebar-item'>";
     echo "<div class='sp-sidebar-item-head'>Suche</div>";
     echo "<div class='sp-sidebar-item-box'>\n";
-        echo "<div class='sp-sidebar-item-box-head'><div class='container'><form><input type='hidden' name='type' value='search'><input type='text' name='term'></form></div></div>\n";
+        echo "<div class='sp-sidebar-item-box-body'><div class='container'><form><input type='hidden' name='type' value='search'><input type='text' name='term'></form></div></div>\n";
     echo "</div>\n";
 echo "</div>";
  
