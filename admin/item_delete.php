1867d9fa190f875186c696687a19a8d1497d0399
zarat <manuel.zarat@gmail.com>
2018-05-27 14:11:08 +0200

    Update item_delete.php

--- admin/item_delete.php
+++ admin/item_delete.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * 
+ * Entfernt ein Item aus der Datenbank.
+ * Die Datei load.php ist eingebunden, weil die Datei asynchron aufgerufen wird.
+ * 
+ */
+
 include "../load.php";
 
 $system = new system();
