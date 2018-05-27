b3c6aaae428fda5068370fb56ade14954a7aaf0e
zarat <manuel.zarat@gmail.com>
2018-05-27 14:08:47 +0200

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,3 +1,11 @@
+<?php
+/**
+ * 
+ * Startseite des Adminbereiches. 
+ * 
+ */
+?>
+
 <script type="text/javascript">function chg(k,id) { go_on = confirm("Diesen Inhalt wirklich entfernen?"); if (go_on) { var xmlhttp = new XMLHttpRequest(); xmlhttp.open("GET","../admin/update_settings.php?setting="+k+"&value="+id,true); xmlhttp.send(); } }</script>
 
 <?php 
