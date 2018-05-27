b52a88606cb4eb7bb33349dac6f39a3961f7a8a6
zarat <manuel.zarat@gmail.com>
2018-05-27 15:00:40 +0200

    Update delete.php

--- admin/menumanager/delete.php
+++ admin/menumanager/delete.php
@@ -1,17 +1,27 @@
 <?php 
 
+/*
+ *
+ * Wird asynchron aufgerufen, deshalb load.php einbinden.
+ * 
+ */
+
 require_once '../../load.php';
 
 $system = new system();
 
 function recursiveDelete($id,$system) {
 
-    $query = $system->select( array( "select" => "*", "from" => "menu", "where" => "parent=$id" ) );
-    print_r($query);
-    //foreach($query as $i => $q) {
-        //recursiveDelete($q['id'],$system);
-    //}
-
+    // Alle unterpunkte finden
+    $get_child_items = $system->select( array( "select" => "id", "from" => "menu", "where" => "parent=$id" ) );
+    
+    foreach($get_child_items as $k => $v) { 
+        $system->delete( array( "from" => "menu", "where" => "id=$v[0]" ) );
+        //echo $v[0] . "<br>";
+        recursiveDelete($v[0], $system);
+    }
+    
+    // Menupunkt selbst entfernen
     $system->delete( array( "from" => "menu", "where" => "id=$id" ) );
 }
 
