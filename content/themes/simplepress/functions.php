557058f399bc6477cb6f376703649957963c74fd
zarat <manuel@zarat.ml>
2018-05-29 01:45:32 +0200

    cleanup and comments

--- content/themes/simplepress/functions.php
+++ content/themes/simplepress/functions.php
@@ -3,14 +3,21 @@
 /**
  * Vorlage einer functions.php
  *
+ * Wird in Klasse theme eingebunden und kann Funktionen darin nutzen.
+ *
  * @author Manuel Zarat
  *
  */
 
-$this->add_action('setup_theme','init_before');
-
-function init_before() { 
+/**
+ * Eine Funktion definieren..
+ */
+function start_session() { 
     session_start();  
 }
+/**
+ * Und in das Theme einhaken.
+ */
+$this->add_action('setup_theme','start_session');
 
 ?>
