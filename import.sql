eb38769780dbac154dec406f729ce28e801bc752
zarat <manuel@zarat.ml>
2018-06-01 23:41:35 +0200

    Update import.sql

--- import.sql
+++ import.sql
@@ -1,5 +1,5 @@
 SET NAMES utf8;
-SET time_zone = '+00:00';
+SET time_zone = '+02:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
@@ -7,8 +7,8 @@ DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `menu_id` int(10) unsigned NOT NULL,
-  `label` varchar(200) CHARACTER SET latin1 NOT NULL DEFAULT '',
-  `link` varchar(100) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `label` varchar(30) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `link` varchar(150) CHARACTER SET latin1 NOT NULL DEFAULT '',
   `parent` int(10) unsigned NOT NULL DEFAULT '0',
   `sort` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
@@ -16,35 +16,35 @@ CREATE TABLE `menu` (
 
 INSERT INTO `menu` (`id`, `menu_id`, `label`, `link`, `parent`, `sort`) VALUES
 (NULL,	1,	'Home',	'../',	0,	1),
-(NULL,	1,	'About',	'../?type=page&id=3',	0,	2);
+(NULL,	1,	'About',	'../?type=page&id=2',	0,	2);
 
 DROP TABLE IF EXISTS `object`;
 CREATE TABLE `object` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(30) NOT NULL,
-  `title` longtext NOT NULL,
-  `keywords` longtext NOT NULL,
-  `description` longtext NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `type` varchar(10) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `keywords` varchar(150) NOT NULL,
+  `description` varchar(150) NOT NULL,
   `content` longtext NOT NULL,
-  `date` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `category` varchar(100) NOT NULL,
+  `date` int(15) NOT NULL,
+  `status` int(1) NOT NULL,
+  `category` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `object` (`id`, `type`, `title`, `keywords`, `description`, `content`, `date`, `status`, `category`) VALUES
-(1,	'category',	'Allgemein',	'',	'',	'Deine erste Kategorie zu allen Allgemeinen Themen',	1491560699,	1,	'1'),
-(2,	'post',	'Ein erster Post',	'',	'',	'Willkommen zu deinem neuen Blog! Das ist dein erster Post, den du jederzeit im Adminbereich bearbeiten oder wieder entfernen kannst. Also los, auf ans bloggen!',	1515107311,	1,	'1'),
-(3,	'page',	'About',	'',	'',	'Seiten werden nicht automatisch in hierarchischen Archiven angelegt.',	1515507779,	1,	'1');
+(1,	'category',	'Allgemein',	'homepage,blog,simplepress', 'Allgemeine Themen', '',	1491560699,	1,	1),
+(2,	'page',	'About',	'homepage,blog,simplepress',	'',	'Hallo auf deiner neuen Homepage. Seiten wie diese werden nicht automatisch in hierarchischen Archiven angelegt. Du kannst sie im Men&uuml;manager anordnen.',	1515507779,	1,	1),
+(3,	'post',	'Dein neuer Blog',	'',	'',	'Willkommen zu deinem neuen Blog! Das ist ein erster Post, den du im <a href="../admin">Adminbereich</a> bearbeiten oder wieder entfernen kannst. Sieh dich dort am besten gleich mal um und dann auf ans bloggen!',	1515107311,	1,	1);
 
 DROP TABLE IF EXISTS `object_meta`;
 CREATE TABLE `object_meta` (
-  `meta_id` int(11) NOT NULL AUTO_INCREMENT,
-  `meta_item_id` int(11) NOT NULL,
+  `meta_id` int(10) NOT NULL AUTO_INCREMENT,
+  `meta_item_id` int(10) NOT NULL,
   `meta_key` varchar(30) NOT NULL,
-  `meta_value` varchar(150) NOT NULL,
+  `meta_value` text NOT NULL,
   PRIMARY KEY (`meta_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `settings`;
 CREATE TABLE `settings` (
@@ -54,8 +54,8 @@ CREATE TABLE `settings` (
 
 INSERT INTO `settings` (`key`, `value`) VALUES
 ('site_title',	'SimplePress'),
-('site_subtitle',	'while(!perfect) { develop() }'),
+('site_subtitle',	'Just another simplepress blog'),
 ('site_keywords',	'CMS, Homepage, Website'),
-('site_description',	'Objektorientiertes CMS in PHP und MySQL'),
+('site_description',	'Simplepress ist ein objektorientiertes CMS in PHP und SQL'),
 ('site_theme',	'simplepress'),
 ('site_language',	'de');
