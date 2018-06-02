780fbc9da5560a063dec2ddd08fd1397f6c600d2
zarat <manuel@zarat.ml>
2018-06-03 01:42:18 +0200

    new db structure

--- import.sql
+++ import.sql
@@ -18,8 +18,8 @@ INSERT INTO `menu` (`id`, `menu_id`, `label`, `link`, `parent`, `sort`) VALUES
 (NULL,	1,	'Home',	'../',	0,	1),
 (NULL,	1,	'About',	'../?type=page&id=2',	0,	2);
 
-DROP TABLE IF EXISTS `object`;
-CREATE TABLE `object` (
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `type` varchar(10) NOT NULL,
   `title` varchar(150) NOT NULL,
@@ -32,13 +32,13 @@ CREATE TABLE `object` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `object` (`id`, `type`, `title`, `keywords`, `description`, `content`, `date`, `status`, `category`) VALUES
+INSERT INTO `item` (`id`, `type`, `title`, `keywords`, `description`, `content`, `date`, `status`, `category`) VALUES
 (1,	'category',	'Allgemein',	'homepage,blog,simplepress', 'Allgemeine Themen', '',	1491560699,	1,	1),
 (2,	'page',	'About',	'homepage,blog,simplepress',	'',	'Hallo auf deiner neuen Homepage. Seiten wie diese werden nicht automatisch in hierarchischen Archiven angelegt. Du kannst sie im Men&uuml;manager anordnen.',	1515507779,	1,	1),
 (3,	'post',	'Dein neuer Blog',	'',	'',	'Willkommen zu deinem neuen Blog! Das ist ein erster Post, den du im <a href="../admin">Adminbereich</a> bearbeiten oder wieder entfernen kannst. Sieh dich dort am besten gleich mal um und dann auf ans bloggen!',	1515107311,	1,	1);
 
-DROP TABLE IF EXISTS `object_meta`;
-CREATE TABLE `object_meta` (
+DROP TABLE IF EXISTS `item_meta`;
+CREATE TABLE `item_meta` (
   `meta_id` int(10) NOT NULL AUTO_INCREMENT,
   `meta_item_id` int(10) NOT NULL,
   `meta_key` varchar(30) NOT NULL,
