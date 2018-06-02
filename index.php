c3d7f9035bf67c87133f55f0b7d30882e0a04240
zarat <manuel@zarat.ml>
2018-06-03 00:20:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,8 @@
 
 /**
  * @author Manuel Zarat
- * @date 05.01.2018
- * @license http://opensource.org/licenses/MIT
- *
- * Klasse core und autoloader einbinden.
- * 
  */
+
 require 'load.php';
 
 $simplepress = new system();
