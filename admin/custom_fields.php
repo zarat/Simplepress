a702e2ec5b7cbd1c8d917130413db5a992a8fe54
zarat <manuel@zarat.ml>
2018-06-03 01:41:05 +0200

    new db structure

--- admin/custom_fields.php
+++ admin/custom_fields.php
@@ -17,15 +17,15 @@ function get_all_fields( $item_id ) {
 
 function add_field( $item_id, $field_key, $field_value ) {
     $system = new system();
-    $config['insert'] = "object_meta (`meta_item_id`, `meta_key`, `meta_value`)";
+    $config['insert'] = "item_meta (`meta_item_id`, `meta_key`, `meta_value`)";
     $config['values'] = " ($item_id, '$field_key', '$field_value')";
     $system->insert( $config );
 }
 
 function delete_field( $field_id ) {
     $system = new system();
-    $item_id = $system->select( array( 'select' => '*', 'from' => 'object_meta', 'where' => 'meta_id=' . $field_id ) );
-    $system->delete( array( 'from' => 'object_meta', 'where' => 'meta_id=' . $field_id ) );
+    $item_id = $system->select( array( 'select' => '*', 'from' => 'item_meta', 'where' => 'meta_id=' . $field_id ) );
+    $system->delete( array( 'from' => 'item_meta', 'where' => 'meta_id=' . $field_id ) );
     echo $item_id[0]['meta_item_id'];
 } 
     
@@ -35,7 +35,7 @@ if( isset( $_POST['item_id'] ) ) {
     $customfield_key = @$_POST['field_key'];
     $customfield_value = @$_POST['field_value'];
     $system = new system();
-    $config = array( "insert" => "object_meta (`meta_item_id`, `meta_key`, `meta_value`)", "values" => "($item_id, '$customfield_key', '$customfield_value')");
+    $config = array( "insert" => "item_meta (`meta_item_id`, `meta_key`, `meta_value`)", "values" => "($item_id, '$customfield_key', '$customfield_value')");
     $last_id = $system->insert($config);
     echo $item_id;
 
