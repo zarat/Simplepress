805c4693224dcf198ddf37f5d6c311dbda4e657e
zarat <manuel@zarat.ml>
2018-06-03 22:57:27 +0200

    Update theme.php

--- system/classes/theme.php
+++ system/classes/theme.php
@@ -5,167 +5,91 @@
  */
 
 class theme extends system {
-    
-    /**
-     * Diese Funktion gibt im Prinzip alles aus. 
-     */
-    final function render() {
-           
-        /**
-         * Diese Funktion muss zuerst ausgefuehrt werden, da die Variablen im Header gebraucht werden. 
-         * Inhalt wird in system gebuffert!
-         */
-        $content = $this->content();
-        
-        /**
-         * Der unsichtbare Header holt sich jetzt die Daten, die davor gebuffert wurden.
-         * zwecks SEO warats
-         */
-        $this->head();
-        
-        /**
-         * Custom scripts & Co bevor der sichtbare Header ausgegeben wird.
-         */
-        $this->before_header();
-        
-        /**
-         * Der sichtbare Header.
-         * Darin ist z.B die Navigation.
-         */
-        $this->header();
-        
-        /**
-         * Custom scripts & Co bevor der sichtbare MAIN CONTENT ausgegeben wird.
-         * Also nach der Navigation aber noch vor dem Inhalt.
-         */
-        $this->before_content(); 
-        
-        /**
-         * Jetzt wird der Inhalt ausgegeben, der zu Beginn gebuffert wurde.
-         */
-        echo $content;
-        
-        /**
-         * Custom scripts & Co die vor der Sidebar ausgegeben werden.
-         */
-        $this->before_sidebar();        
-        
-        /**
-         * Die Sidebar
-         */
-        $this->sidebar();       
-        
-        /**
-         * Custom scripts & Co, die vor dem sichbaren Footer ausgegeben werden.
-         */
-        $this->before_footer();
-        
-        /**
-         * Der sichtbare Footer.
-         */
-        $this->footer();
-        
-        /**
-         * Der unsichtbare Footer
-         */
-        $this->foot(); 
-           
-    }
   
     /**
-     * Der unsichtbare Header
-     * Bindet die ganzen Metatags & Co vor </head> ein
+     * Der HTML Header ist der im Browser unsichtbare Bereich der die ganzen Metatags & Co einbindet
      */
-    final function head() { 
-        
+    final function html_header() { 
+            
         echo "<!DOCTYPE html>\n<html>\n";
         echo "<head>\n";
 
-        $item = false;
-        $item = @$this->get_current_item();
-        
+        /**
+         * Wenn ein Item abgefragt wurde, werden hier bereits die Metatags geaendert und muessen davor beschafft werden. 
+         */
+        $item = $this->get_current_item();        
         $title = $item['title'] ? $this->settings( 'site_title' ) . " - " . $item['title'] :$this->settings( 'site_title' );
         $keywords = $item['keywords'] ? $item['keywords'] : $this->settings( 'site_keywords' );
         $description = $item['description'] ? $item['description'] : $this->settings( 'site_description' );
         
-        echo "<title>" . $title . "</title>\n";        
-        
+        echo "<title>" . $title . "</title>\n";                
         echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />\n";
         echo "<meta name='viewport' content='width=device-width, initial-scale=1.0' />\n";        
         echo "<meta name='generator' content='SimplePress - https://github.com/zarat/simplepress' />\n";
         echo "<meta name='keywords' content='$keywords'>\n";
-        echo "<meta name='description' content='$description'>\n";
-        
+        echo "<meta name='description' content='$description'>\n";        
         echo "<link rel='stylesheet' href='../content/themes/" . $this->settings( 'site_theme' ) . "/css/style.css'>\n";
-        echo "<link rel='stylesheet' href='../content/themes/" . $this->settings( 'site_theme' ) . "/css/menu.css'>\n"; 
-             
+        echo "<link rel='stylesheet' href='../content/themes/" . $this->settings( 'site_theme' ) . "/css/menu.css'>\n";              
         echo "</head>\n";
         echo "<body>\n";
         
     }
-    
+
     /**
-     * Wenn etwas ganz am Anfang des HTML Markup stehen soll kommt es hierhin.
-     * 
-     * @todo
+     * Der Header ist der sichtbare, oberste Bereich der Seite.
      */
-    function before_header() { }
-    
-    /**
-     * Header 
-     * 
-     * Damit ist der im Browser sichtbare Teil nach <body> gemeint
-     * 
-     * @todo Was tun, wenn kein Menue erstellt wurde?!!!
-     * 
-     */    
     function header() {
-         $nav = new menu();
-         $nav->config(array('id' => 1));
-         echo $nav->html();         
+        echo "<div class=\"sp-main-wrapper\">";
+        echo "<div class='sp-main-header'>\n";
+        echo "<div class='sp-main-header-logo'><h1>".$this->settings('site_title')."</h1><h4>".$this->settings('site_subtitle')."</h4></div>\n";
+        echo "</div>\n";         
     }
     
     /**
-     * Wenn etwas vor dem Hauptinhalt angezeigt werden soll, kommt es hierhin
+     * Die Hauptnavigation ist in spezielle BeginNoIndex-Tags gewrappt. 
+     * Diese sollen SuMa sagen, das der darin stehende Content keinen direkten Bezug zum gerade angezeigten Context hat.
+     * Z.b eine Liste mit Kategorien oder den neuesten Items.
+     * Diese haben ja nicht unbedingt mit dem aktuellen Context zu tun.
      * 
-     * @todo
-     * 
+     * @todo multiple menues - Wenn man eine ID uebergibt kann man eine andere laden
      */
-    function before_content() { }
+    function navigation( $id = 1 ) {
+         $nav = new menu();
+         $nav->config( array( 'id' => $id ) );
+         echo "<!--BeginNoIndex-->";
+         echo $nav->html(); 
+         echo "<!--EndNoIndex-->";   
+    }
     
     /**
-     * Wenn etwas vor der Sidebar angezeigt werden soll, kommt es hierhin
-     * 
-     * @todo
-     * 
-     */
-    function before_sidebar() { }
-    
-    /**
-     * Wenn etwas unter dem ganzen Inhalt angezeigt werden soll, kommt es hierhin
-     * 
-     * @todo
-     * 
-     */
-    function before_footer() { }
-    
-    /**
-     * Footer
-     * Mit footer ist hier der sichtbare Footer gemeint, also der untere Teil der Website.
-     * 
+     * Der Footer ist der sichtbare, unterste Bereich der Seite.
      */
     function footer() {    
-        echo "Powered by <a href='https://github.com/zarat/simplepress' target='_blank'>Simplepress</a> | <a href='../rss.php'>RSS</a>";        
+        echo "Powered by <a href='https://github.com/zarat/simplepress' target='_blank'>Simplepress</a> | <a href='../rss.php'>RSS</a>";
+        echo "<div/>";        
     }
     
     /**
-     * HTML Footer, also der Teil nach </html>
-     * Eigentlich nur fuer Style- und Scriptincludes
-     * 
+     * Der HTML Footer steht im HTML ganz unten und schliesst es auch.
      */
-    final function foot() {    
+    final function html_footer() {    
         echo "</body>\n";
         echo "</html>";
+    }
+    
+    /**
+     * Diese Funktion setzt im Prinzip alles zusammen und gibt es aus. 
+     * Sie kann im Custom Theme ueberschrieben werden.     
+     */
+    function render() {
+        $content = $this->content();        
+        $this->html_header();
+        $this->header();
+        $this->navigation();
+        echo $content;
+        $this->sidebar();       
+        $this->footer();
+        $this->html_footer();            
     }
     
     /**
