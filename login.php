8e3afe3d414f56265b7e11fd1c124b0d91e9b77a
zarat <manuel@zarat.ml>
2018-06-03 04:32:40 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,14 @@
 <?php
 
 /**
- * Login script triple-alpha
- *
  * @author Manuel Zarat
- *
  */
-
-session_start();
 
 include "load.php";
 
-$formpass=NULL;
-$formlogin=NULL;
+require ("config.php"); 
+
 ?>
-<?php require ("config.php"); ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 
 <html>
@@ -59,7 +53,7 @@ $nav->html();
 <center>
 <table cellspacing="4" cellpadding="4" style="">
 	<tr>
-		<td>Username</td>
+		<td>Email</td>
 		<td><input type="text" name="formlogin" class="cssborder" autofocus></td>
 
 		<td>Passwort</td>
@@ -70,48 +64,58 @@ $nav->html();
 </center> 	 
 </form>
 
-<?php } else { ?>
+<?php } else { 
 
-<?php
-	$formpass = $_POST['formpass'];
-	$formlogin = $_POST['formlogin'];
-  
-if ($formpass == $adminpass && $formlogin == $adminlogin) {
+    $formlogin = $_POST['formlogin'];
+    $formpass = md5($_POST['formpass']);      
 
-	$_SESSION["loggedin"] = 1;
-	//session_register("loggedin");
-	//$loggedin = "1";
-	//logged in so run a javascript redirect to admin page.
-?>
-<script language="javascript">
-<!-- 
-location.replace("admin");
--->
-</script>
-	<h4><a href='admin'>Du bist jetzt angemeldet</a></h4>
-<?php
+    $system = new system();    
+
+    if( $user = $system->auth($formlogin,$formpass) ) {
+    
+    $token = md5( $user . time() );
+    $cfg = array( "table" => "user", "set" => "token='$token' where id=" . $user );
+    $system->update( $cfg );    
+        /**
+         * Cookie clientseitig setzen wg Zeitzonen Offset
+         */
+        echo "
+        <script>
+        function setCookie(name, value, hours) {
+            var d = new Date();
+            d.setTime(d.getTime() + (hours*60*60*1000));
+            var expires = \"expires=\" + d.toUTCString();
+            document.cookie = name + \"=\" + value + \";\" + expires + \";path=/\";
+        }
+        setCookie('sp-uid','$token',1); 
+        </script>";
+        
+        echo "Du wurdest erfolgreich angemeldet.";
+        
+    } else {
+    
+        echo "Anmeldeversuch gescheitert.";
+        
+    }
+
 }
-}
+
 ?>
 
 </div>
 
-<!-- Sidebar -->
 <div class='sp-sidebar'>
 
-<div class="sp-sidebar-item-box">
-<div class="sp-sidebar-item-box-head">Hinweis</div>
-<div class="sp-sidebar-item-box-body">Achte immer darauf, das dir keiner beim anmelden zusieht!</div>
-</div>
+    <div class="sp-sidebar-item-box">
+        <div class="sp-sidebar-item-box-head">Hinweis</div>
+        <div class="sp-sidebar-item-box-body">Achte immer darauf, das dir keiner beim anmelden zusieht!</div>
+    </div>
 
 </div>
-<!-- Sidebar Ende -->
 
 <div style='clear:both;'></div><!-- Der Footer -->
-<div class="sp-footer" style="padding:10px;"></div>
 
-<!-- Der Wrapper aus dem Header wird geschlossen -->
-</div>
+<div class="sp-footer" style="padding:10px;"></div>
 
 </div>
 
