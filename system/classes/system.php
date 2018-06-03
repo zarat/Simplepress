43010bb62bc50b7666b83b1490e9b76fe700ecb1
zarat <manuel@zarat.ml>
2018-06-03 22:57:08 +0200

    Update system.php

--- system/classes/system.php
+++ system/classes/system.php
@@ -217,23 +217,21 @@ class system extends core {
             include $custom_include_file;                    
         } else if( is_file( $include_file ) ) {                
             include $include_file;                   
-        } else {             
-            echo "<div class='sp-sidebar'>";                   
-                echo "<div class='sp-sidebar-item'>";
-                    echo "<div class='sp-sidebar-item-head'>Suche</div>";
+        } else {                                
+            echo "<div class='sp-sidebar-item'>";
+                echo "<div class='sp-sidebar-item-head'>Suche</div>";
+                echo "<div class='sp-sidebar-item-box'>\n";
+                    echo "<div class='sp-sidebar-item-box-body'><div class='container'><form><input type='hidden' name='type' value='search'><input type='text' name='term'></form></div></div>\n";
+                echo "</div>\n";
+            echo "</div>";                 
+            echo "<div class='sp-sidebar-item'>";
+                echo "<div class='sp-sidebar-item-head'>Kategorien</div>";
+                foreach($this->archive( array('select' => 'id,title','from' => 'item','where' => 'status=1 AND type="category"') ) as $cat) {
                     echo "<div class='sp-sidebar-item-box'>\n";
-                        echo "<div class='sp-sidebar-item-box-body'><div class='container'><form><input type='hidden' name='type' value='search'><input type='text' name='term'></form></div></div>\n";
+                        echo "<div class='sp-sidebar-item-box-head'><a href='../?type=category&id=$cat[id]'>$cat[title]</a></div>\n";
                     echo "</div>\n";
-                echo "</div>";                 
-                echo "<div class='sp-sidebar-item'>";
-                    echo "<div class='sp-sidebar-item-head'>Kategorien</div>";
-                    foreach($this->archive( array('select' => 'id,title','from' => 'item','where' => 'status=1 AND type="category"') ) as $cat) {
-                        echo "<div class='sp-sidebar-item-box'>\n";
-                            echo "<div class='sp-sidebar-item-box-head'><a href='../?type=category&id=$cat[id]'>$cat[title]</a></div>\n";
-                        echo "</div>\n";
-                    }
-                echo "</div>"; 
-            echo "</div>";                      
+                }
+            echo "</div>";                       
         }                              
     }
 
