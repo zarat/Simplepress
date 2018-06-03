f397bf568e4a95d00e3a532261233f8347b5ef5b
zarat <manuel@zarat.ml>
2018-06-03 23:23:25 +0200

    Update single.php

--- content/themes/simplepress/single.php
+++ content/themes/simplepress/single.php
@@ -4,14 +4,10 @@
  * @author Manuel Zarat
  */ 
 
-echo "<div class='sp-content'>\n";
-
 echo "<div class ='sp-content-item'>\n";
-echo "<div class ='sp-content-item-head'>" . $item['title'] . "</div>\n";
-echo "<div class ='sp-content-item-head-secondary'>" . date("d.m.y",$item['date']) . "</div>\n";
-echo "<div class ='sp-content-item-body'>" . $item['content'] . "</div>\n";
-echo "</div>\n";
-
+    echo "<div class ='sp-content-item-head'>" . $item['title'] . "</div>\n";
+    echo "<div class ='sp-content-item-head-secondary'>" . date("d.m.y",$item['date']) . "</div>\n";
+    echo "<div class ='sp-content-item-body'>" . $item['content'] . "</div>\n";
 echo "</div>\n";
 
 ?>
