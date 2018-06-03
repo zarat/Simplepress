d861f60d39d97646962a5c44a85ba7e2aa9061eb
zarat <manuel@zarat.ml>
2018-06-03 22:56:37 +0200

    Update theme.php

--- content/themes/simplepress/theme.php
+++ content/themes/simplepress/theme.php
@@ -1,30 +1,10 @@
 <?php
 
 /**
- * Simplepress Theme
- *
  * @author Manuel Zarat
- * 
  */
 
 class simplepress extends theme {
-
-    function nav() {
-        $nav = new menu();
-        $nav->config(array('id' => 1));
-        $nav->html();
-    }
-    
-    function before_header() {
-        echo "<div class='sp-main-wrapper'>\n";
-    }
-    
-    function header() {
-        echo "<div class='sp-main-header'>\n";
-        echo "<div class='sp-main-header-logo'><h1>".$this->settings('site_title')."</h1><h4>".$this->settings('site_subtitle')."</h4></div>\n";
-        echo "</div>\n";
-        parent::header();
-    }
     
     function sidebar() {
         echo "<div class='sp-sidebar'>\n";
@@ -51,12 +31,6 @@ class simplepress extends theme {
         parent::footer();
         echo "</div>";
         echo "</div>";
-    }
-    
-    function before_content() {
-        if(!$this->request('type')) {
-            //echo "<iframe width=\"100%\" height=\"315\" src=\"https://www.youtube.com/embed/VWIQmgApIZI\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>";
-        }
     }
     
 }
