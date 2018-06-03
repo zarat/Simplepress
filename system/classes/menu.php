13a82e33e717778de50d13a85c2d39f681878edb
zarat <manuel@zarat.ml>
2018-06-03 21:24:04 +0200

    Update menu.php

--- system/classes/menu.php
+++ system/classes/menu.php
@@ -11,69 +11,48 @@ private $div;
 private $ul; 
 private $sublevel = 0;
 
-    /** 
-     * Laedt das Menue
-     *
-     * @param array $config
-     * @return void
-     */
     function config($config) {
         $this->menu_id = $config['id'];
         $this->div = "nav-container";
         $this->ul = "submenu";
     }
-    
-    /**
-     * Rekursive Funktion, die sich selbst immer und immer wieder aufruft
-     * 
-     * @param id int
-     * @return string
-     */
+
     protected function items($id=0) {
         $query = array('select' => '*','from' => 'menu','where' => "menu_id=" . $this->menu_id . " AND parent=$id ORDER BY sort");
-        if($parents = $this->archive($query)) {
-        
-            if($this->sublevel<1) { echo "\n" . str_repeat("\t", $this->sublevel) . "<ul class='menu level-$this->sublevel'>\n"; } /** Noch kin Submenu!!! */
-            else { echo "\n" . str_repeat("\t", $this->sublevel) . "<ul class='$this->ul level-$this->sublevel'>\n"; } 
-            $this->sublevel = $this->sublevel+1;  
-            foreach($parents as $item) {    
-                echo str_repeat("\t", $this->sublevel) ."<li><a href='$item[link]'>$item[label]</a>";
-                if($children = $this->items($item['id'])) {
-                    echo $children;
-                }
-                echo str_repeat("\t", $this->sublevel) . "</li>\n";
-            } 
-            $this->sublevel = $this->sublevel-1;
-            /**
-             * Adminlink bei letzter Iteration
-             * 
-             */
-            if($this->sublevel<1 && $this->auth() ) { 
-            
-                echo str_repeat("\t", $this->sublevel) . "<li><a href='../admin'>Admin</a>\n";
-                echo "<ul>";
-                echo str_repeat("\t", $this->sublevel) . "<li><a href='../logout.php'>logout</a></li>\n";
-                echo "</ul></li>";
-                
-            }       
-            echo str_repeat("\t", $this->sublevel) . "</ul>\n";
-            
+        if($parents = $this->archive($query)) {                
+            if($this->sublevel<1) { 
+                echo "<ul class='menu level-$this->sublevel'>\n";  
+            } else { 
+                echo "\n" . str_repeat("\t", $this->sublevel) . "<ul class='$this->ul level-$this->sublevel'>\n"; 
+            }             
+            $this->sublevel++;              
+            foreach($parents as $item) {                
+                echo str_repeat("\t", $this->sublevel) ."<li><a href='$item[link]'>$item[label]</a>";                   
+                if($children = $this->items($item['id'])) {                
+                    echo $children;                    
+                }                 
+                echo str_repeat("\t", $this->sublevel) . "</li>\n";                  
+            }             
+            $this->sublevel--;            
+            if($this->sublevel < 1 && $this->auth() ) {             
+                echo "<li><a href='../admin'>Admin</a>\n";
+                echo "<ul>\n";
+                echo "<li><a href='../logout.php'>logout</a></li>\n";
+                echo "</ul>\n";
+                echo "</li>\n";                
+            }                   
+            echo str_repeat("\t", $this->sublevel) . "</ul>\n";                        
         }
     }
-    
-    /**
-     * Anzeigen
-     * 
-     * @return string
-     */
+
     public function html() {    
-        echo "<div class='$this->div'>";
+        echo "<div class='$this->div'>\n";
         echo "<label class='responsive_menu' for='responsive_menu'>";
         echo "<span>Menu</span>";
-        echo "</label>";
-        echo "<input id='responsive_menu' type='checkbox'>"; 
+        echo "</label>\n";
+        echo "<input id='responsive_menu' type='checkbox'>\n"; 
         $this->items();                  
-        echo "</div>\n\n";
+        echo "</div>\n";
     }
 
 }
