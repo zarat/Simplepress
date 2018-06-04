d507b21703af62bccb6e9512126959e1863c01b3
zarat <manuel@zarat.ml>
2018-06-04 16:07:10 +0200

    Update delete.php

--- admin/menumanager/delete.php
+++ admin/menumanager/delete.php
@@ -1,30 +1,29 @@
 <?php 
 
 /*
- *
- * Wird asynchron aufgerufen, deshalb load.php einbinden.
- * 
+ * @author Manuel Zarat
  */
 
 require_once '../../load.php';
+require_once "auth.php";
 
 $system = new system();
 
-function recursiveDelete($id,$system) {
+function recursiveDelete( $id, $system ) {
 
     // Finden aller Unterpunkte..
     $get_child_items = $system->select( array( "select" => "id", "from" => "menu", "where" => "parent=$id" ) );
     
     // Jeden hierarchisch darunter liegenden REKURSIV!!! entfernen
-    foreach($get_child_items as $k => $v) { 
-        $system->delete( array( "from" => "menu", "where" => "id=$v[0]" ) );
-        recursiveDelete($v[0], $system);
+    foreach( $get_child_items as $item => $values ) { 
+        $system->delete( array( "from" => "menu", "where" => "id=$values[0]" ) );
+        recursiveDelete( $values[0], $system );
     }
     
     // Und den Menupunkt selbst entfernen
     $system->delete( array( "from" => "menu", "where" => "id=$id" ) );
 }
 
-recursiveDelete($_POST['id'],$system);
+recursiveDelete( $_POST['id'], $system );
 
 ?>
