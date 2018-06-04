daa40340ef2028bb9dcc5f5188f977b0efc9e3d1
zarat <manuel@zarat.ml>
2018-06-04 16:04:09 +0200

    Update update_settings.php

--- admin/update_settings.php
+++ admin/update_settings.php
@@ -1,24 +1,17 @@
 <?php
 
-/*
- *
+/**
  * Asynchrones Speichern der Daten von dashboard.php
- *
- * @ToDo: Uebergebene Werte pruefen!
- *
  */
 
-include("../load.php");
-include("auth.php");
+require_once "../load.php";
+require_once "auth.php";
 
 $system = new system();
 
 $setting = $_GET['setting'];
 $value = $_GET['value'];
 
-if($setting == "site_theme") {
-    $cfg=array("table"=>"settings","set"=>"value='$value' WHERE settings.key='$setting'");
-    $system->update($cfg);
-}
+$system->update( array( "table" => "settings", "set" => "value='$value' WHERE settings.key='$setting'" ) );
 
 ?>
