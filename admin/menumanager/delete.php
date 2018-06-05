d6e58a30e31fb64d62cec3a11a7324d326286f19
zarat <manuel@zarat.ml>
2018-06-05 12:19:03 +0200

    Subitem fix
    Subitems werden nicht mehr mit gelöscht sondern auf die nächst höhere Ebene verschoben.

--- admin/menumanager/delete.php
+++ admin/menumanager/delete.php
@@ -5,23 +5,34 @@
  */
 
 require_once '../../load.php';
-require_once "auth.php";
 
 $system = new system();
+
+if( !$system->auth() ) { die( 'Nope' ); }
 
 function recursiveDelete( $id, $system ) {
 
     // Finden aller Unterpunkte..
-    $get_child_items = $system->select( array( "select" => "id", "from" => "menu", "where" => "parent=$id" ) );
+    $child_items = $system->select( array( "select" => "id", "from" => "menu", "where" => "parent=$id" ) );    
+    
+    // Finden des Oberpunktes..
+    $get_parent_item = $system->select( array( "select" => "parent", "from" => "menu", "where" => "id=$id" ) );
+    $parent_item = $get_parent_item[0];
     
     // Jeden hierarchisch darunter liegenden REKURSIV!!! entfernen
-    foreach( $get_child_items as $item => $values ) { 
-        $system->delete( array( "from" => "menu", "where" => "id=$values[0]" ) );
-        recursiveDelete( $values[0], $system );
+    foreach( $child_items as $item ) { 
+
+        $temp_item_id = $item['id'];
+                
+        //$system->delete( array( "from" => "menu", "where" => "id=$id" ) );
+        $system->update( array( "table" => "menu", "set" => "parent=$parent_item[parent] where id=$temp_item_id" ) );
+        recursiveDelete( $id, $system );
+        
     }
     
     // Und den Menupunkt selbst entfernen
     $system->delete( array( "from" => "menu", "where" => "id=$id" ) );
+    
 }
 
 recursiveDelete( $_POST['id'], $system );
