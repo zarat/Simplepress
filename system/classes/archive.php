77f411841a7fd7bac5d79ba4e8a353ed5a763234
zarat <manuel@zarat.ml>
2018-06-05 12:22:08 +0200

    Update archive.php

--- system/classes/archive.php
+++ system/classes/archive.php
@@ -6,17 +6,18 @@
 
 class archive extends system {
 
-private $posts = [];
 private $max_per_page = 10;
 private $displayed_this_page = 0;
 private $last = 0;
-private $post_count = 0;
+private $item_count = 0;
 
-    function archive_init() {
-        if( !empty( $this->request( 'last' ) ) ) { 
-            $this->last = $this->request( 'last' );
-        }
-        $this->fill_posts(); 
+public $items = [];
+
+    function archive_init() {    
+        if( !empty( $this->request( 'last' ) ) ) {         
+            $this->last = $this->request( 'last' );            
+        }        
+        $this->fill_items();         
     }
     
     /**
@@ -24,71 +25,74 @@ private $post_count = 0;
      * 
      * @todo Suche & Blaettern
      */
-    final function fill_posts() { 
-        if( $this->request( 'type' ) && $this->request( 'type' ) == 'category' ) {
-            if( $this->request( 'last' ) ) {      
-                $this->posts = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type='post' AND category='" . $this->request( 'id' ) . "' AND id < " . $this->request( 'last' ) . " ORDER BY id ASC") );                 
+    final function fill_items() {     
+        if( $this->request( 'type' ) && $this->request( 'type' ) == 'category' ) {        
+            if( $this->request( 'last' ) ) {   
+                $this->items = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type='post' AND category='" . $this->request( 'id' ) . "' AND id < " . $this->request( 'last' ) . " ORDER BY id ASC") );                             
+            } else {                                   
+                $this->items = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type='post' AND category='" . $this->request( 'id' ) . "' ORDER BY id ASC") );                           
+            }            
+        } elseif( $this->request( 'type' ) && $this->request( 'type' ) == 'search' ) {             
+            if( $this->request( 'last' ) ) {                      
+                $this->items = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type IN ('page','post') AND ( title LIKE '%" . htmlentities( $this->request( 'term' ) ) . "%' OR content LIKE '%" . htmlentities( $this->request( 'term' ) ) . "%' ) AND id < " . $this->request( 'last' ) . " ORDER BY id ASC") );                             
             } else {                   
-                $this->posts = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type='post' AND category='" . $this->request( 'id' ) . "' ORDER BY id ASC") );               
-            } 
-        } elseif( $this->request( 'type' ) && $this->request( 'type' ) == 'search' ) { 
-            if( $this->request( 'last' ) ) {      
-                $this->posts = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type IN ('page','post') AND ( title LIKE '%" . htmlentities( $this->request( 'term' ) ) . "%' OR content LIKE '%" . htmlentities( $this->request( 'term' ) ) . "%' ) AND id < " . $this->request( 'last' ) . " ORDER BY id ASC") );                 
-            } else {                   
-                $this->posts = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type IN ('page','post') AND ( title LIKE '%" . htmlentities( $this->request( 'term' ) ) . "%' OR content LIKE '%" . htmlentities( $this->request( 'term' ) ) . "%' ) ORDER BY id ASC") );               
-            }                                
-        } else {   
-            if( $this->request( 'last' ) ) {                     
-                $this->posts = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type='post' AND id < " . $this->request( 'last' ) . " ORDER BY id ASC") );                  
-            } else {                    
-                $this->posts = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type='post' ORDER BY id ASC") );                
+                $this->items = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type IN ('page','post') AND ( title LIKE '%" . htmlentities( $this->request( 'term' ) ) . "%' OR content LIKE '%" . htmlentities( $this->request( 'term' ) ) . "%' ) ORDER BY id ASC") );               
+            }                                            
+        } else {  
+            if( $this->request( 'last' ) ) {    
+                $this->items = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type='post' AND id < " . $this->request( 'last' ) . " ORDER BY id ASC") );                  
+            } else {  
+                $this->items = $this->select( array( "select" => "*", "from" => "item", "where" => "status=1 AND type='post' ORDER BY id ASC") );                
             }  
         }  
-        $this->post_count = sizeof( $this->posts );                   
+        $this->post_count = sizeof( $this->items );   
     }
 
-    function count_posts() {    
-        return ($this->posts);
+    function count_items() { 
+        return ($this->items);        
     }
 
-    function have_posts() {                
-        if( $this->displayed_this_page >= $this->max_per_page )  {        
-            $this->is_page_limit = true;            
-            return false;            
-        }    
-        return ( count($this->posts) > 0) ? true : false;    
+    function have_items() {                    
+        if( $this->displayed_this_page >= $this->max_per_page ) {                
+            $this->is_page_limit = true;                        
+            return false;                        
+        }            
+        return ( count($this->items) > 0) ? true : false;            
     }
     
-    function more() {
-        return ( count($this->posts) > 0) ? true : false;
+    function more() {    
+        return ( count($this->items) > 0) ? true : false;        
     }
 
-    function the_post( $strip_tags = false, $content_length = false ) {        
-        if( $this->more() ) {
-            $post = array_pop( $this->posts );        
-            $this->last = $post['id'];
-            $this->displayed_this_page++;         
-            if($strip_tags ) { $post['content'] = strip_tags($post['content']); }        
-            if($content_length) {
-                $line=$post['content'];
-                if (preg_match('/^.{1,'.$content_length.'}\b/s', $post['content'], $match)) {
-                    $post['content'] = $match[0];
-                }
-            }                   
-            return $post; 
-        }
-        return false;       
+    function the_item( $strip_tags = false, $content_length = false ) {            
+        if( $this->more() ) {        
+            $post = array_pop( $this->items );                    
+            $this->last = $post['id'];            
+            $this->displayed_this_page++; 
+            if($strip_tags ) {             
+                $post['content'] = strip_tags($post['content']);                 
+            }                    
+            if($content_length) {            
+                $line=$post['content'];                
+                if (preg_match('/^.{1,'.$content_length.'}\b/s', $post['content'], $match)) {                
+                    $post['content'] = $match[0];                   
+                }               
+            }                               
+            return $post;             
+        }        
+        return false;               
     }
 
-    function pagination() {  
-        if( $this->more() ) {               
-            if( $this->request( 'type' ) == 'category' ) {                          
-                echo "<div class='sp-content-item'><div class='sp-content-item-head'><a rel='nofollow' href='../?type=category&id=" . $this->request( 'id' ) . "&last=" . $this->last . "'>&auml;ltere Beitr&auml;ge</a></div></div>";                                                    
-            } else {
-                echo "<div class='sp-content-item'><div class='sp-content-item-head'><a rel='nofollow' href='../?last=" . $this->last . "'>&auml;ltere Beitr&auml;ge</a></div></div>";
-            }                      
-        }                         
+    function pagination() {      
+        if( $this->more() ) {                       
+            if( $this->request( 'type' ) == 'category' ) {                                      
+                echo "<div class='sp-content-item'><div class='sp-content-item-head'><a rel='nofollow' href='../?type=category&id=" . $this->request( 'id' ) . "&last=" . $this->last . "'>&auml;ltere Beitr&auml;ge</a></div></div>";                                                                
+            } else {                
+                echo "<div class='sp-content-item'><div class='sp-content-item-head'><a rel='nofollow' href='../?last=" . $this->last . "'>&auml;ltere Beitr&auml;ge</a></div></div>";           
+            }                              
+        }                            
     }
+
 }
 
 ?>
