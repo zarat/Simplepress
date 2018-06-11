bffd9134610ab5b94edb92a7e211bfc0f9600362
zarat <manuel@zarat.ml>
2018-06-12 00:53:14 +0200

    Update single.php

--- content/themes/simplepress/single.php
+++ content/themes/simplepress/single.php
@@ -4,15 +4,15 @@
  * @author Manuel Zarat
  */ 
 
-echo "<div class ='sp-content-item'>\n";
-    echo "<div class ='sp-content-item-head'>" . $item['title'] . "</div>\n";
-    echo "<div class ='sp-content-item-head-secondary'>";
-    echo date("d.m.Y",$item['date']);
-    if( $this->auth() ) {
-        echo " - <a href=\"../admin/?page=item_modify&id=$item[id]\">bearbeiten</a>";
-    }
+while( $item->have_items() ) {
+
+    $item1 = $item->the_item( $strip_tags=true, $content_length=200 );
+    
+    echo "<div class='sp-content-item'>\n";
+    echo "<div class='sp-content-item-head'><a href='../?type=$item1[type]&id=$item1[id]'>" . $item1['title'] . "</a></div>\n";
+    echo "<div class='sp-content-item-body'>" . ( $item1['content'] ) . "</div>\n";
     echo "</div>\n";
-    echo "<div class ='sp-content-item-body'>" . $item['content'] . "</div>\n";
-echo "</div>\n";
+    
+}
 
 ?>
