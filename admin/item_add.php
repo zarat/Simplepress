7a1b07c59ea902fb0fc2f8b90cba8ade08c41565
zarat <manuel@zarat.ml>
2018-06-12 02:02:27 +0200

    Update item_add.php

--- admin/item_add.php
+++ admin/item_add.php
@@ -9,122 +9,95 @@ if( !@$_GET['type'] && !@$_POST['title'] ) { die("sorry, wrong query."); }
 $posttype = $_GET['type'];
 
 if(!empty($_POST['title'])) { 
-
-    $title =            !empty( $_POST['title'] ) ? htmlentities($_POST['title'], ENT_QUOTES, 'utf-8') : "";
-    $keywords =         !empty( $_POST['keywords'] ) ? htmlentities($_POST['keywords'], ENT_QUOTES, 'utf-8') : "";  
-    $description =      !empty( $_POST['description'] ) ? htmlentities($_POST['description'], ENT_QUOTES, 'utf-8') : "";
-    $text =             !empty( $_POST['text'] ) ? htmlentities($_POST['text'], ENT_QUOTES, 'utf-8') : "";
-    $category =         !empty( $_POST['category'] ) ? $_POST['category'] : 0;
-    $date =             !empty($_POST['date']) ? strtotime( $_POST['date'] ) : time();
+	
+    $title = !empty( $_POST['title'] ) ? htmlentities($_POST['title'], ENT_QUOTES, 'utf-8') : "";
+    $keywords = !empty( $_POST['keywords'] ) ? htmlentities($_POST['keywords'], ENT_QUOTES, 'utf-8') : "";  
+    $description = !empty( $_POST['description'] ) ? htmlentities($_POST['description'], ENT_QUOTES, 'utf-8') : "";
+    $category = !empty( $_POST['category'] ) ? $_POST['category'] : 0;
+    $date = !empty($_POST['date']) ? strtotime( $_POST['date'] ) : time();
+    
+	/**
+	 * Strip htmlentities between <pre> Tags??
+     *
+	function strip_pre_content($matches) {
+		return str_replace($matches[1],htmlentities($matches[1], ENT_QUOTES),$matches[0]);
+	}
+	$text = preg_replace_callback('/<pre>(.*?)<\/pre>/imsu','strip_pre_content', $_POST['text']);
+    */
+    
+    $text = base64_decode( $_POST['text'] );
         
     $cfg = array("insert"=>"item (type, title, content, description, keywords, status, category, date)","values"=>"('$posttype','$title', '$text', '$description', '$keywords', 1, $category, $date)");
     $system->insert($cfg);
-    
-    $last = $system->last_insert_id();
-      
-    echo "Dein Inhalt wurde gespeichert. Du kannst ihn <a href='../?type=$posttype&id=". $last . "'>hier ansehen</a> oder <a href='../admin/?page=item_modify&id=" . $last . "'>weiter bearbeiten</a>.";
 	
 } else {
 
-echo "<link rel=\"stylesheet\" href=\"../admin/css/datepicker.css\">\n";
-echo "<link href=\"../admin/css/suneditor.css\" rel=\"stylesheet\" type=\"text/css\">\n";
-echo "<script type=\"text/javascript\" src=\"../admin/js/suneditor.js\"></script>\n";
+echo "<script type=\"text/javascript\" src=\"../admin/js/admin.js\"></script>\n";
+echo "<script type=\"text/javascript\" src=\"../admin/js/jswriter.js\"></script>\n";
 echo "<script type=\"text/javascript\" src=\"../admin/js/datepicker.js\"></script>\n";
+echo "<link rel=\"stylesheet\" href=\"../admin/css/datepicker.css\">\n";
+echo "<link rel=\"stylesheet\" href=\"../admin/css/jswriter.css\">\n";
 
-/**
- * SP Content Anfang
- */
-echo "<div class=\"sp-content\">";
+?>
 
-    /**
-     * SP Content Item Anfang
-     */
-    echo "<div class=\"sp-content-item\">";
+<div class="sp-content">
+<div class="sp-content-item">
+<div class="sp-content-item-head"><?php echo $system->_t('item_add'); ?></div>
+<div class="sp-content-item-body">
 
-    echo "<div class='sp-content-item-head'>" . $system->_t('item_add') . "</div>";
-
-    /**
-     * SP Content Item Body Anfang
-     */
-    echo "<div class=\"sp-content-item-body\">";
-
-    /**
-     * FORM Anfang
-     */
-    echo "<form id=\"frm\" method=\"post\">";
-    
-    echo '<p>' . $system->_t('item_add_title') . ' <a onclick="toggle(\'more\');" href="#">weitere Optionen</a></p>'; 
-    echo "<p><input name=\"title\" type=\"text\"></p>";
-    
-    /**
-     * DIV More Anfang
-     */
-    echo "<div id=\"more\" style=\"display:none;\">";
-    
-        echo '<p>' . $system->_t('item_modify_date') . '</p>'; 
-        echo "<p><input type=\"text\" name=\"date\" class=\"datepicker\"></p>";
-        
-        echo '<p>' . $system->_t('item_add_category') . '</p>';
-        echo "<p><select name=\"category\">";
-        $cfg = array('select'=>'*','from'=>'item','where'=>'type="category"');
-        $a = $system->archive($cfg);
-        for($i=0;$i<count($a);$i++) {
-            if($result['category'] == $a[$i]['id']) {
-                echo "<option value='" . $a[$i]['id'] . "' selected='selected'>" . $a[$i]['title'] . "</option>";
-            } else {
+<form id="frm" method="post">
+    <p>Titel <a onclick="toggle('more');" href="#">weitere Optionen</a></p>    
+    <p><input name="title" type="text" value="" id="title"></p>    
+    <div id="more" style="display:none;">    
+        <p>Datum</p>
+        <p><input type="text" name="date" class="datepicker" value="" id="date"></p>        
+        <p>Kategorie</p>
+        <p><select name="category">
+            <?php
+            $cfg = array('select'=>'*','from'=>'item','where'=>'type="category"');
+            $a = $system->archive($cfg);
+            for($i=0;$i<count($a);$i++) {
                 echo "<option value='" . $a[$i]['id'] . "'>" . $a[$i]['title'] . "</option>";
             }
-        }
-        echo "</select></p>";
-        
-        echo '<p>' . $system->_t('item_add_keywords') . '</p>'; 
-        echo "<p><input name=\"keywords\" type=\"text\"></p>";
-        
-        echo '<p>' . $system->_t('item_add_description') . '</p>'; 
-        echo "<p><input name=\"description\" type=\"text\"></p>";
-    
-    /**
-     * DIV More Ende
-     */
-    echo "</div>";
-    
-    echo '<p>' . $system->_t('item_add_content') . '</p>';
-    echo "<p><textarea cols=\"40\" rows=\"20\" name=\"text\" id=\"editor\"></textarea></p>";
-    
-    echo "<p><a style=\"cursor:pointer;\" onclick=\"sun_save();\">Item speichern</a></p>";
-    
-    /**
-     * FORM Ende
-     */
-    echo "</form>";
+            ?>
+        </select></p>        
+        <p>Schlagworte</p>
+        <p><input name="keywords" type="text" value="" id="keywords"></p>        
+        <p>Beschreibung</p><p><input name="description" type="text" value="" id="description"></p>   
+    </div>   
+    <p>Inhalt</p>
+    <p><div id="editor"></div></p>
+    TXT:<div id="text-output"></div>
+    <br><br>
+    HTML:<div id="html-output"></div>
+    <p><a style="cursor:pointer;" onclick="save();">Item speichern</a></p>    
+</form>
 
-}
+</div>
+</div>
+</div>
 
-/**
- * SP Content Item Body Ende
- */
-echo "</div>";
+<div style="clear:both;"></div>
 
-/**
- * SP Content Item Ende
- */
-echo "</div>";
-
-/**
- * SP Content Ende
- */
-echo "</div>";
-
-echo "<div style=\"clear:both;\"></div>";
-
-?> 
 <script>
-var suneditor = SUNEDITOR.create('editor', {
-    //imageUploadUrl:"upload.php"
-});
-document.getElementById("datepicker").datepicker();
-function sun_save() {
-    suneditor.save();
-    document.getElementById('frm').submit();
-};
+  var editor = window.jswriter.init({
+    element: document.getElementById('editor'),
+    defaultParagraphSeparator: 'p',
+    styleWithCSS: false,
+    onChange: function (html) {
+      document.getElementById('text-output').textContent = html
+      document.getElementById('html-output').innerHTML = html
+    }
+  })  
+
+  function save() {
+    var title = document.getElementById('title').value;
+    var date = document.getElementById('date').value;
+    var keywords = document.getElementById('keywords').value;
+    var description = document.getElementById('description').value;
+    var content = btoa(document.getElementById('text-output').innerHTML);
+    ajaxpost("index.php?page=item_add&type=<?php echo $posttype; ?>", "title=" + title + "&date=" + date + "&keywords=" + keywords + "&description=" + description + "&category=1&text=" + content );
+  }
 </script>
+
+<?php } ?>
