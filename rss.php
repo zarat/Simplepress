597e4d8ec6606db06d1ee862d0ddc9626804732d
zarat <manuel@zarat.ml>
2018-06-14 19:57:01 +0200

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,5 +1,4 @@
 <?php 
-
 /**
  * Simplepress RSS 2.0 Feed
  *
@@ -8,43 +7,46 @@
  * @link https://github.com/zarat/simplepress   
  * @since 06/2018 
  */
-
 header("Content-type: text/xml");
-
 require_once "load.php";
-
 echo "<?xml version=\"1.0\" encoding=\"utf-8\" " . "?" . ">";
 echo "<rss version=\"2.0\">";
-
 $system = new system(); 
-
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 $channel_url = $protocol . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 $item_url = str_replace(basename(__file__), '', $channel_url);
-
 echo "<channel>";
 echo "<title>" . $system->settings('site_title') . " > Updates</title>";
 echo "<link>" . $channel_url . "</link>";
 echo "<description>" . html_entity_decode($system->settings('site_description')) . "</description>";
-
 $cfg = array("select"=>"*","from"=>"item","where"=>"type='post' AND status=1 ORDER BY id DESC");
 $rss = $system->archive($cfg);
     
 foreach($rss as $row)    {
     
     echo "<item>";
-    echo "<title>" . html_entity_decode($row['title']) . "</title>";
+    echo "<title>" . strip_tags( html_entity_decode( $row['title'] ) ) . "</title>";
+    
+    /**
+     * Links muesen kodiert werden um im RSS richtig dargestellt zu werden
+     */
     echo "<link>" . htmlspecialchars($item_url . "?type=" . $row['type'] . "&id=" . $row['id']) . "</link>";
-    if ( strlen( strip_tags( html_entity_decode( $row['content'] ) ) ) > 240 ) {
-        $row['content'] = preg_replace("/[^ ]*$/", '', substr( html_entity_decode( $row['content'] ), 0, 240) ); 
-    }
-    echo "<description>" . $row['content'] . "</description>";
-    echo "<pubDate>" . date('r', $row['date']) . "</pubDate>"; /** moderne RSS Feeds haben RFC822 konformes Datum! */
+    
+    /**
+     * Inhalt kuerzen, wenn laenger als n Zeichen, Woerter dabei ganz lassen!
+     */
+    $content = strip_tags( html_entity_decode( $row['content'] ) );
+    if ( strlen( $content ) > 240 ) {
+        $content = preg_replace("/[^ ]*$/", '', substr( $content, 0, 240) ); 
+    } 
+    echo "<description>" . htmlspecialchars($content) . "</description>";
+    
+    /** 
+     * moderne RSS Feeds haben RFC822 konformes Datum! 
+     */
+    echo "<pubDate>" . date('r', $row['date']) . "</pubDate>";
     echo "</item>";
-
 }
-
 echo "</channel>";
 echo "</rss>";
-
 ?>
