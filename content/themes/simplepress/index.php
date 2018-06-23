1c5243dffeff15d449b20b17e3c0ae9c7f8148de
zarat <manuel@zarat.ml>
2018-06-23 04:28:17 +0200

    Update index.php

--- content/themes/simplepress/index.php
+++ content/themes/simplepress/index.php
@@ -11,7 +11,7 @@ while( $archive->have_items() ) {
     $item = $archive->the_item( array( "content_length" => 240 ) );
     
     echo "<div class='sp-content-item'>\n";
-    echo "<div class='sp-content-item-head'><a href='../?type=$item[type]&id=$item[id]'>" . $item['title'] . "</a></div>\n";
+    echo "<div class='sp-content-item-head'><a href='../?id=$item[id]'>" . $item['title'] . "</a></div>\n";
     echo "<div class='sp-content-item-body'>" . $item['content'] . "</div>\n";
     echo "</div>\n";
     
