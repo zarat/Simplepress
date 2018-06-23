be09be28703fb0b4ff0ddd344ff877cdde038e11
zarat <manuel@zarat.ml>
2018-06-24 00:41:44 +0200

    Update theme.php

--- system/classes/theme.php
+++ system/classes/theme.php
@@ -168,7 +168,7 @@ class theme extends system {
             echo "<div class='sidebar-item'>\n";
                 echo "<div class='sidebar-item-head'>Suche</div>\n";
                 echo "<div class='sidebar-item-box'>\n";
-                    echo "<div class='sidebar-item-box-body'><div class='container'><form><input type='hidden' name='type' value='search'><input type='text' name='term'></form></div></div>\n";
+                    echo "<div class='sidebar-item-box-body'><div class='container'><form><input type='text' name='search'></form></div></div>\n";
                 echo "</div>\n";
             echo "</div>\n";                        
         }
