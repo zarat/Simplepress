83a0c80d647f9cb9b2d3cb134c95a317c89d7ea6
zarat <manuel@zarat.ml>
2018-06-23 06:18:16 +0200

    Update sidebar.php

--- content/themes/simplepress/sidebar.php
+++ content/themes/simplepress/sidebar.php
@@ -3,14 +3,22 @@
 /**
  * @author Manuel Zarat 
  */
- 
-/**
- * Nur ein kleines Suchfeld
- */
+
 echo "<div class='sp-sidebar-item'>";
     echo "<div class='sp-sidebar-item-head'>Suche</div>";
     echo "<div class='sp-sidebar-item-box'>\n";
         echo "<div class='sp-sidebar-item-box-body'><div class='container'><form><input type='hidden' name='type' value='search'><input type='text' name='term'></form></div></div>\n";
+    echo "</div>\n";
+echo "</div>";
+
+$cat_query = "select * from term WHERE taxonomy_id=(SELECT id FROM term_taxonomy WHERE taxonomy='category')";
+$cats = $this->fetch_all_assoc( $this->query( $cat_query ) );
+echo "<div class='sp-sidebar-item'>";
+    echo "<div class='sp-sidebar-item-head'>Kategorien</div>";
+    echo "<div class='sp-sidebar-item-box'>\n";
+    foreach($cats as $cat) {
+        echo "<div class='sp-sidebar-item-box-body'><a href='../?category=$cat[id]'>$cat[name]</a></div>";
+    }
     echo "</div>\n";
 echo "</div>";
 
