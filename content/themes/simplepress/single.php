3256ed61b16b520dea8d02f5c1599dad12c00061
zarat <manuel@zarat.ml>
2018-06-23 20:12:09 +0200

    Update single.php

--- content/themes/simplepress/single.php
+++ content/themes/simplepress/single.php
@@ -13,7 +13,7 @@ while( $archive->have_items() ) {
         echo "<div class ='sp-content-item-head-secondary'>";
         echo date("d.m.Y",$item['date']);
         if( $this->auth() ) {
-            echo " - <a href=\"../admin/item.php?action=modify&id=$item[id]\">bearbeiten</a>";
+            echo " - <a href=\"../admin/item.php?action=edit&id=$item[id]\">bearbeiten</a>";
         }
         echo "</div>\n";
         echo "<div class ='sp-content-item-body'>" . $item['content'] . "</div>\n";
