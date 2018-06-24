b637fdd9052bc3b13080504f17c0654a8ea2cc7b
zarat <manuel@zarat.ml>
2018-06-24 19:08:15 +0200

    Update delete.php

--- admin/item/delete.php
+++ admin/item/delete.php
@@ -8,16 +8,23 @@ require "../../load.php";
 
 $system = new system();
 
+if( !$system->auth() ) die();
+
 $id = $_GET['id'];
 
 /**
- * Das Item selbst
+ * Das Item
  */
-$system->delete(array("from"=>"item","where"=>"id=$id"));
+$system->delete( array( "from" => "item", "where" => "id=$id" ) );
 
 /**
- * und die Custom fields dazu entfernen.
+ * Die Custom fields
  */
-$system->delete(array("from"=>"item_meta","where"=>"meta_item_id=$id"));
+$system->delete( array( "from" => "item_meta", "where" => "meta_item_id=$id" ) );
+
+/**
+ * Die Relationen
+ */
+$system->delete(array("from"=>"term_relation","where"=>"object_id=$id"));
 
 ?>
