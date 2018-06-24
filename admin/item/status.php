e942f16c468d46969e36d9c1695e427f9fee19a8
zarat <manuel@zarat.ml>
2018-06-24 19:17:51 +0200

    Update status.php

--- admin/item/status.php
+++ admin/item/status.php
@@ -1,9 +1,11 @@
 <?php
 
 /**
+ * Die Datei wird asynchron aufgerufen deshalb ist $system noch nicht definiert!
+ * Asynchron eingebundene Dateien muessen load.php einbinden und auch $system deklarieren!
+ *
  * @author Manuel Zarat
  */
-
 require_once "../../load.php";
 
 $system = new system();
@@ -11,8 +13,7 @@ $system = new system();
 if( !$system->auth() ) { header("Location: ../login.php"); }
 
 $id = $_GET['id'];
-$status = $_GET['status'];
-    
+$status = $_GET['status'];   
 $newstatus = ($status==1) ? 0 : 1;
 $response = ($status==1) ? "deaktivieren" : "aktivieren";
     
