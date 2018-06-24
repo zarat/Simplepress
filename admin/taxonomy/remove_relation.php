0600d5da2435a7beb097efc69abc681d8dcc06eb
zarat <manuel@zarat.ml>
2018-06-24 19:37:25 +0200

    Update remove_relation.php

--- admin/taxonomy/remove_relation.php
+++ admin/taxonomy/remove_relation.php
@@ -3,12 +3,15 @@
 /**
  * @author Manuel Zarat
  */
-    $tax = $_GET['taxonomy'];
-    $itemid = $_GET['item_id'];
-    $term = $_GET['term'];
-    /**
-     * Taxonomie aus db entfernen
-     */
-    $id = $system->query( "delete from term_relation where object_id=$itemid and taxonomy_id=$tax and term_id=$term" );
+if( !$system->auth() ) header("Location: ../login.php");
+
+$tax = $_GET['taxonomy'];
+$itemid = $_GET['item_id'];
+$term = $_GET['term'];
+
+/**
+ * Taxonomie aus db entfernen
+ */
+$id = $system->query( "delete from term_relation where object_id=$itemid and taxonomy_id=$tax and term_id=$term" );
 
 ?>
