f65dfa14f298283812f1d845b809be398d0f0c52
zarat <manuel@zarat.ml>
2018-06-24 19:36:05 +0200

    Update add_relation.php

--- admin/taxonomy/add_relation.php
+++ admin/taxonomy/add_relation.php
@@ -3,12 +3,15 @@
 /**
  * @author Manuel Zarat
  */
-    $tax = $_GET['taxonomy'];
-    $itemid = $_GET['item_id'];
-    $term = $_GET['term'];
-    /**
-     * Neue Taxonomie speichern
-     */
-    $id = $system->query( "insert into term_relation (object_id,taxonomy_id,term_id) values ($itemid, $tax, $term)" );
+if( !$system->auth() ) header("Location: ../login.php"); 
+ 
+$tax = $_GET['taxonomy'];
+$itemid = $_GET['item_id'];
+$term = $_GET['term'];
+
+/**
+ * Neue Taxonomie speichern
+ */
+$id = $system->query( "insert into term_relation (object_id,taxonomy_id,term_id) values ($itemid, $tax, $term)" );
 
 ?>
