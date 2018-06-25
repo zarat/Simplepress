be4a24308fe2aa4a11ddb98f51e75feb448b7b4b
zarat <manuel@zarat.ml>
2018-06-26 01:39:38 +0200

    tax update 2

--- system/classes/term.php
+++ system/classes/term.php
@@ -12,6 +12,7 @@ class term extends system {
 
     /**
      * Alle Terms die in der Tabelle term existieren
+     * @deprecated die spalte taxonomy_id
      */
     function terms() {
         $query = "
@@ -20,6 +21,16 @@ class term extends system {
             ";
         $result = $this->fetch_all_assoc( $this->query( $query ) ); 
         return $result;    
+    }
+
+    function by_taxonomy_id( $taxonomy_id ) {
+        $query = "
+            select term.* from term
+            join term_relation tr on tr.term_id =term.id
+            where tr.taxonomy_id=$taxonomy_id
+            ";
+        $result = $this->fetch_all_assoc( $this->query( $query ) ); 
+        return $result;
     }
     
     function terms_by_taxonomy_id( $taxonomy_id ) {
@@ -32,13 +43,15 @@ class term extends system {
         return $result;
     }
     
+    /**
+     * Wird im Widget bei item edit verwendet um die Formularfelder zu selektieren..
+     * @deprecated
+     */
     function terms_by_item_id( $item_id, $parent_taxonomy ) {
         $query = "
-            select * from term
-            inner join term_relation tr on tr.object_id=$item_id
-            inner join term_taxonomy tt on term.taxonomy_id=tt.id
-            where term.taxonomy_id=$parent_taxonomy 
- 
+            select * from term where id in(
+                select term_id from term_relation where object_id=$item_id and taxonomy_id=$parent_taxonomy
+            )  
             ";
         $result = $this->fetch_all_assoc( $this->query( $query ) ); 
         return $result;
