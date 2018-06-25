24accf72d108d3678a7f983848f197cea48dae2c
zarat <manuel@zarat.ml>
2018-06-26 01:47:13 +0200

    Update sidebar.php

--- content/themes/simplepress/sidebar.php
+++ content/themes/simplepress/sidebar.php
@@ -11,15 +11,4 @@ echo "<div class='sp-sidebar-item'>";
     echo "</div>\n";
 echo "</div>";
 
-$cat_query = "select * from term WHERE taxonomy_id=(SELECT id FROM term_taxonomy WHERE taxonomy='category')";
-$cats = $this->fetch_all_assoc( $this->query( $cat_query ) );
-echo "<div class='sp-sidebar-item'>";
-    echo "<div class='sp-sidebar-item-head'>Kategorien</div>";
-    echo "<div class='sp-sidebar-item-box'>\n";
-    foreach($cats as $cat) {
-        echo "<div class='sp-sidebar-item-box-body'><a href='../?category=$cat[id]'>$cat[name]</a></div>";
-    }
-    echo "</div>\n";
-echo "</div>";
-
 ?>
