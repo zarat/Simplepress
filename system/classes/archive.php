144499e9af6c725fd4384d41a3e56df8e3df3ed1
zarat <manuel@zarat.ml>
2018-06-26 16:58:42 +0200

    Update archive.php

--- system/classes/archive.php
+++ system/classes/archive.php
@@ -46,11 +46,24 @@ public $is_search = false;
         if( false !== $config ) {            
             $this->items = $this->select( $config );            
         } else {                    
-            $query = "";             
+            $query = ""; 
+            if ( $this->request( 'category' ) ) {
+                $cat_query = "                    
+                        select item.* from item 
+                        inner join term_relation tr on tr.object_id=item.id
+                        inner join term_taxonomy tt on tt.id=tr.taxonomy_id
+                        inner join term t on t.id=tr.term_id
+                        where tr.taxonomy_id=(
+                        	select id from term_taxonomy where taxonomy='category'
+                        )
+                        AND t.id='" . $this->request( 'category' ) . "'
+                        ";
+                $query = $cat_query;                
+                $this->is_archive = true;         
             /**
              * Suche ist eine hierarchische Taxonomie.. Mehr dazu kommt noch 
              */
-            if( $this->request( 'search' ) ) {
+            } else if( $this->request( 'search' ) ) {
                 $where_search = "select * from item WHERE ( title LIKE '%" . htmlentities( $this->request( 'search' ) ) . "%' OR content LIKE '%" . htmlentities( $this->request( 'search' ) ) . "%' ) ";
                 $where_search = $hooks->apply_filters('archive_init_search', $where_search);
                 $query = $where_search;  
@@ -74,10 +87,11 @@ public $is_search = false;
                         GROUP_CONCAT( 
                             ( SELECT taxonomy FROM term_taxonomy WHERE id=tr.taxonomy_id ), 
                             '_', 
-                            ( tr.term_id ) 
+                            ( t.id ) -- by name | by id bei taxonomien unterschiedlich 
                         ) AS type
                         FROM item
                         JOIN term_relation tr ON tr.object_id=item.id
+                        join term t on t.id=tr.term_id
                         GROUP BY item.id
                         HAVING type LIKE ('%$key"."_"."$val%')
                             ";
