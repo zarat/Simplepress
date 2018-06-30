137ec5ec9733556a48f52c73a39737d552700984
zarat <manuel@zarat.ml>
2018-06-30 04:27:19 +0200

    Update status.php

--- admin/item/status.php
+++ admin/item/status.php
@@ -17,7 +17,7 @@ $status = $_GET['status'];
 $newstatus = ($status==1) ? 0 : 1;
 $response = ($status==1) ? "deaktivieren" : "aktivieren";
     
-$system->update( array( "table"=>"item", "set"=>"status=$status WHERE id=$id" ) );
+$system->query( "update item set status=$status WHERE id=$id" );
 
 /**
  * Weil die Datei asynchron aufgerufen wird, wird hier ein Rueckgabewert ausgegeben.
