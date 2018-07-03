a1636946d67800b0b50cc8bb783c7efada1ab0d1
zarat <manuel@zarat.ml>
2018-07-04 01:11:24 +0200

    Update archive.php

--- system/classes/archive.php
+++ system/classes/archive.php
@@ -107,10 +107,7 @@ public $is_search = false;
                     $ps[] = " ( type_int LIKE ('$p') OR type_str LIKE ('$p') ) "; 
                 }                                                            
                 /** 
-                 * Klammer = Wichtig!!! 
-                 * AND = alle muessen passen
-                 * OR = irgendeines muss passen
-                 * Default = OR
+                 * AND = alle muessen passen, OR = irgendeines muss passen 
                  */
                 $custom_query .= "HAVING ( " . implode(" OR ", $ps ) . " ) ";
                 if( $this->request('last') ) $custom_query .= "AND item.date < " . $this->request('last') . " ";
