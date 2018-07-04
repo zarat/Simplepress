f662d37f03ecca15f9d1a93d3ea753dc1dda46f8
zarat <manuel@zarat.ml>
2018-07-05 00:15:19 +0200

    Update add.php

--- admin/taxonomy/add.php
+++ admin/taxonomy/add.php
@@ -12,9 +12,8 @@ if( !empty( $_POST['name'] ) ) {
     /**
      * speichern
      */
-    $name = $_POST['name'];
-    $parent = isset($_POST['parent']) ? $_POST['parent'] : 0;    
-    $id = $system->query( "insert into term_taxonomy (taxonomy, parent) values ('$name', $parent)" );
+    $name = $_POST['name'];    
+    $id = $system->query( "insert into term_taxonomy (taxonomy) values ('$name')" );
 	
 }
 
@@ -33,18 +32,7 @@ if( !empty( $_POST['name'] ) ) {
     <div class="sp-content-item-body">
     
         <form id="frm" method="post">
-       
-            <p>Parent?</p>
-            <select name="parent">
-            <option value="0" selected="selected">Waehle</option>
-            <?php            
-            foreach( $system->taxonomies() as $taxonomy ) {
-                echo "<option value='" . $taxonomy['id'] . "'>" . $taxonomy['taxonomy'] . "</option>";
-            }  
-            
-            ?>   
-            </select></p>
-                    
+    
             <p>Name</p>
             <p><input type="text" name="name"></p>            
             
