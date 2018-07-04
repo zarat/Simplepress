49da93efc7813f2f5da751b1ee947d17e6defa3b
zarat <manuel@zarat.ml>
2018-07-04 23:46:51 +0200

    Update single.php

--- content/themes/simplepress/single.php
+++ content/themes/simplepress/single.php
@@ -9,6 +9,7 @@ while( $archive->have_items() ) {
     $item = $archive->the_item( );
     
     echo "<div class ='sp-content-item'>\n";
+    
         echo "<div class ='sp-content-item-head'>" . $item['title'] . "</div>\n";
         echo "<div class ='sp-content-item-head-secondary'>";
         echo date("d.m.Y",$item['date']);
@@ -16,7 +17,16 @@ while( $archive->have_items() ) {
             echo " - <a href=\"../admin/item.php?action=edit&id=$item[id]\">bearbeiten</a>";
         }
         echo "</div>\n";
+    
         echo "<div class ='sp-content-item-body'>" . $item['content'] . "</div>\n";
+    
+        if( $item_tags = $this->terms( 'tag', $item['id'] ) ) {
+            foreach( $item_tags as $tag ) { 
+                $tags[] = "<a href='../?tag=$tag[id]'>$tag[name]</a>"; 
+            }
+            echo "<div class ='sp-content-item-body'>Tags: " . implode(', ', $tags) . "</div>";
+        } 
+    
     echo "</div>\n";
     
 }
