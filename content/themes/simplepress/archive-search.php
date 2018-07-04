330896bfe0970af190ad29a39fe4ec5346d9c04f
zarat <manuel@zarat.ml>
2018-07-04 23:52:52 +0200

    Create archive-search.php

---
+++ content/themes/simplepress/archive-search.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @author Manuel Zarat
+ */
+
+echo "<!--BeginNoIndex-->\n";
+
+echo "<div class='sp-content-item'>\n";
+echo "<div class='sp-content-item-head'>Suchergebnisse</div>\n";
+echo "</div>\n";
+
+while( $archive->have_items() ) {
+
+    $item = $archive->the_item( array( "content_length" => 240, 'strip_tags' => true ) );
+    
+    echo "<div class='sp-content-item'>\n";
+    echo "<div class='sp-content-item-head'><a href='../?id=$item[id]'>" . $item['title'] . "</a></div>\n";
+    echo "<div class='sp-content-item-body'>" . $item['content'] . "</div>\n";
+    echo "</div>\n";
+    
+}
+
+$archive->pagination();
+
+echo "<!--EndNoIndex-->\n";
+
+?>
