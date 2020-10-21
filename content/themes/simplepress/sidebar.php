022ee1b5349406f44a5703bab9bca59c542f4059
Manuel Zarat <manuel@zarat.ml>
2020-10-21 06:22:51 +0200

    Update sidebar.php

--- content/themes/simplepress/sidebar.php
+++ content/themes/simplepress/sidebar.php
@@ -7,7 +7,8 @@
 echo "<div class='sp-sidebar-item'>";
     echo "<div class='sp-sidebar-item-head'>Suche</div>";
     echo "<div class='sp-sidebar-item-box'>\n";
-        echo "<div class='sp-sidebar-item-box-body'><div class='container'><form><input type='text' name='search'></form></div></div>\n";
+        $searchVal = $this->request("search") ? " value='" . $this->request("search") . "'" : "";
+        echo "<div class='sidebar-item-box-body'><div class='container'><form><input type='text' name='search'" . $searchVal . "></form></div></div>\n";
     echo "</div>\n";
 echo "</div>";
 
