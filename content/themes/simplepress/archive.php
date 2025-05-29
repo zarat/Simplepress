610ba3a348a43b0e07bc9bca4f38289158bcd48b
Manuel Zarat <manuel@zarat.ml>
2025-05-29 06:20:01 +0200

    Update archive.php

--- content/themes/simplepress/archive.php
+++ content/themes/simplepress/archive.php
@@ -8,11 +8,12 @@ echo "<!--BeginNoIndex-->\n";
 
 while( $archive->have_items() ) {
 
-    $item = $archive->the_item( array( "content_length" => 240, 'strip_tags' => true ) );
+    $item = $archive->the_item( array( "content_length" => 240, 'strip_tags' => false ) );
     
     echo "<div class='sp-content-item'>\n";
     echo "<div class='sp-content-item-head'><a href='../?id=$item[id]'>" . $item['title'] . "</a></div>\n";
-    echo "<div class='sp-content-item-body'>" . $item['content'] . "</div>\n";
+    $content = $item["content"];
+    echo "<div class='sp-content-item-body'>" . preg_replace('#<script(.*?)>(.*?)</script>#is', '', $content) . "</div>\n";
     echo "</div>\n";
     
 }
