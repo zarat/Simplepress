95918b796017b5d43d3c1814affba8c2f34c9e24
Manuel Zarat <manuel@zarat.ml>
2025-05-29 06:20:17 +0200

    Update index.php

--- content/themes/simplepress/index.php
+++ content/themes/simplepress/index.php
@@ -8,7 +8,7 @@ echo "<!--BeginNoIndex-->\n";
 
 while( $archive->have_items() ) {
 
-    $item = $archive->the_item( array( "content_length" => 240, 'strip_tags' => true ) );
+    $item = $archive->the_item( array( "content_length" => 240, 'strip_tags' => false ) );
     
     echo "<div class='sp-content-item'>\n";
     echo "<div class='sp-content-item-head'><a href='../?id=$item[id]'>" . $item['title'] . "</a></div>\n";
